c40692cd9891d612628ee51841bf3167ca30ff48
anukraticodes <146018357+anukraticodes@users.noreply.github.com>
2024-06-10 10:46:05 +0530

    Time: 4 ms (32.68%), Space: 10.1 MB (44.04%) - LeetHub

--- 1137-height-checker/1137-height-checker.cpp
+++ 1137-height-checker/1137-height-checker.cpp
@@ -3,15 +3,7 @@ public:
     int heightChecker(vector<int>& heights) {
         int cnt=0;
         vector<int> v=heights;
-        for(int i=0; i<heights.size()-1; i++){
-            for(int j=i+1; j<heights.size(); j++){
-                if(heights[i]>heights[j]){
-                        int temp=heights[i];
-                        heights[i]=heights[j];
-                        heights[j]=temp;
-                }
-            }
-        }
+        sort(heights.begin(), heights.end());
         for(int i=0; i<heights.size(); i++){
             if(v[i]!=heights[i])
             cnt++;
