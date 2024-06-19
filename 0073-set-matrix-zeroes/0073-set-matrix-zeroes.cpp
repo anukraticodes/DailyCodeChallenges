216cffcac3ca3aa1a106fbe676935458a42e9742
Anukrati Chaturvedi <146018357+anukraticodes@users.noreply.github.com>
2024-06-19 19:41:58 +0530

    Time: 17 ms (10.72%), Space: 16.8 MB (8.91%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,30 +1,28 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-         set<int> r, c;
-        int n=matrix.size(), m=matrix[0].size();
-        
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                if(matrix[i][j]==0)
-                {
-                    r.insert(i), c.insert(j);
+        int m=matrix.size(), n=matrix[0].size();
+        set<int> r,c;
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(matrix[i][j]==0){
+                    r.insert(i);
+                    c.insert(j);
                 }
             }
         }
-        
-        for(auto it=r.begin(); it!=r.end(); it++){
-            for(int j=0; j<m; j++){
-                matrix[*it][j]=0;
+
+        for(int i=0; i<n; i++){
+            for(auto ele: r){
+                matrix[ele][i]=0;
             }
         }
-        
-        for(auto it=c.begin(); it!=c.end(); it++){
-            for(int j=0; j<n; j++){
-                matrix[j][*it]=0;
+
+        for(int i=0; i<m; i++){
+            for(auto ele: c){
+                matrix[i][ele]=0;
             }
         }
-        
-        return;
+
     }
 };
