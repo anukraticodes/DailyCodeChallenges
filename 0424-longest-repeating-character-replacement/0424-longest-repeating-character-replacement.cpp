3ef64fc1cd121cd70e2442be5ca62407eddf469e
Anukrati Chaturvedi <146018357+anukraticodes@users.noreply.github.com>
2024-07-15 14:37:31 +0530

    Time: 16 ms (18.48%), Space: 9.7 MB (8.73%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -3,17 +3,17 @@ public:
     int characterReplacement(string s, int k) {
         map<char, int> mp;
         int maxf=0; int r=0; int l=0; int len=INT_MIN;
-        for(r=0; r<s.size(); r++){
+        while(r<s.size()){
             mp[s[r]]++;
             maxf=max(maxf, mp[s[r]]);
 
-            if((r-l+1)-maxf>k){
+            while((r-l+1)-maxf>k){
                 mp[s[l]]--;
                 l++;
             }
-            else{
+            
                 len=max(len, r-l+1);
-            }
+            r++;
         }
         
         return len;
