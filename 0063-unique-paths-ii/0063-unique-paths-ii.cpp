9317a23afe6339802c341ee52da4e9a1308050ff
Anukrati Chaturvedi <146018357+anukraticodes@users.noreply.github.com>
2024-10-03 10:33:28 +0530

    Time: 6 ms (21.12%), Space: 10.5 MB (40.84%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -8,14 +8,19 @@ public:
     }
 
 int helper(int m, int n, vector<vector<int>>& grid, vector<vector<int>>& dp){
-    if(grid[m][n]==1) return 0;
-    // if(m>0 && n>0 && grid[m][n]==1) return 0;
-    if(m==0&&n==0) return 1;
-    if(m<0 || n<0) return 0;
-    if(dp[m][n]!=-1) return dp[m][n];
-   int up=(m>0)?helper(m-1, n, grid,dp):0;
-    int left=(n>0)?helper(m, n-1, grid,dp):0;
-    return dp[m][n]=up+left;
+    dp[0][0]=1;
+   for(int i=0; i<=m; i++){
+        for(int j=0; j<=n; j++){
+            if(i==0 && j==0) continue;
+             if (grid[i][j] == 1) {
+                dp[i][j] = 0;continue;}
+             int up=0, left=0;
+            if(i>0) up=dp[i-1][j];
+            if(j>0) left=dp[i][j-1];
+            dp[i][j]=up+left;
+        }
+    }
+    return dp[m][n];
 }
 
 };
