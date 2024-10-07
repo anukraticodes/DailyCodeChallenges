40001499007f4385c7cc9f218a6169ad9988d4df
Anukrati Chaturvedi <146018357+anukraticodes@users.noreply.github.com>
2024-10-07 19:18:03 +0530

    Time: 16 ms (22.74%), Space: 13.3 MB (95.11%) - LeetHub

--- 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.cpp
+++ 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.cpp
@@ -1,31 +1,19 @@
 class Solution {
 public:
-    int minFallingPathSum(vector<vector<int>>& m) {
-        int n=m.size();
-        if(m.size()==0) return -1;
-        vector<vector<int>> dp(n, vector<int>(n,-1));
-        for(int i=0; i<n; i++){
-            helper(m,dp,0,i,n);
-        }
-        int ans=INT_MAX;
-        for(int i=0; i<n; i++){
-            ans=min(ans, dp[0][i]);
-        }
-        return ans;
-    }
-
-
-    void helper(vector<vector<int>>& m,vector<vector<int>>& dp, int i, int j, int n){
-        for(int i=0; i<n; i++){
-            dp[n-1][i]=m[n-1][i];
-        }
-        for(int i=n-2; i>=0; i--){
-            for(int j=0; j<n; j++){
-                int down=j>=0&&j<=n-1?dp[i+1][j]:INT_MAX;
-                int right=j+1>=0&&j+1<=n-1?dp[i+1][j+1]:INT_MAX;
-                int left=j-1>=0&&j-1<=n-1?dp[i+1][j-1]:INT_MAX;
-                dp[i][j]=min({down, left, right})+m[i][j];
+    int minFallingPathSum(vector<vector<int>>& matrix) {
+        // int res = INT_MAX;
+        for(int i=1; i<matrix.size(); i++) {
+            for(int x=0; x<matrix[i].size(); x++) {
+                int prev = matrix[i-1][x];
+                if(x>0) prev = min(prev, matrix[i-1][x-1]);
+                if(x<matrix[i].size()-1) prev = min(prev, matrix[i-1][x+1]);
+                matrix[i][x] += prev;
             }
         }
+        vector<int> arr;
+        arr.swap(matrix[matrix.size()-1]); 
+        sort(arr.begin(), arr.end());
+        return arr[0];
+        
     }
 };
