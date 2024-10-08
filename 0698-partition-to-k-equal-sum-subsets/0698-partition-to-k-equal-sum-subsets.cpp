2cabcc69c71a427942325e71a1026ef660ce0f64
Anukrati Chaturvedi <146018357+anukraticodes@users.noreply.github.com>
2024-10-08 13:44:20 +0530

    Time: 3 ms (94.28%), Space: 11.3 MB (72.67%) - LeetHub

--- 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.cpp
+++ 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.cpp
@@ -1,24 +1,34 @@
 class Solution {
 public:
-    bool canPartitionKSubsets(vector<int>& nums, int k) {
-        
-        int sum = accumulate(nums.begin(), nums.end(), 0);
-        if (sum % k != 0) return false;
-        int target = sum / k;
-        int n = nums.size();
-        sort(nums.rbegin(), nums.rend());
-        vector<int> dp(1 << n, -1);
-        dp[0] = 0;
-        for (int mask = 0; mask < (1 << n); ++mask) {
-            if (dp[mask] == -1) continue;
-            for (int i = 0; i < n; ++i) {
-                if (!(mask & (1 << i)) && dp[mask] + nums[i] <= target) {
-                    int newMask = mask | (1 << i);
-                    dp[newMask] = (dp[mask] + nums[i]) % target;
-                    if (dp[newMask] == 0 && newMask == (1 << n) - 1) return true;
+    bool solver(vector<int>& nums, vector<int>& visited, int start_index, int k, int current_sum, int target) {
+        if (k == 0) return true;  
+        if (current_sum == target) {
+            
+            return solver(nums, visited, 0, k - 1, 0, target);
+        }
+
+        for (int i = start_index; i < nums.size(); i++) {
+            if (!visited[i] && current_sum + nums[i] <= target) {
+                visited[i] = 1;  
+                if (solver(nums, visited, i + 1, k, current_sum + nums[i], target)) {
+                    return true;
                 }
+                visited[i] = 0;  
+               if (current_sum == 0)
+                    return false;
             }
-        }  
-        return dp[(1 << n) - 1] == 0;
+        }
+
+        return false;
+    }
+
+    bool canPartitionKSubsets(vector<int>& nums, int k) {
+        int sum = accumulate(nums.begin(), nums.end(), 0);
+        if (sum % k != 0) return false;  
+
+        int target = sum / k;
+        vector<int> visited(nums.size(), 0);
+        sort(nums.begin(), nums.end(), greater<int>()); 
+        return solver(nums, visited, 0, k, 0, target);
     }
 };
