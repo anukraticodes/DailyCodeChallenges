31647156b42907458006ad68fd730382e2ba51aa
Anukrati Chaturvedi <146018357+anukraticodes@users.noreply.github.com>
2024-10-10 16:53:29 +0530

    Time: 51 ms (24.91%), Space: 79.9 MB (5.1%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,21 +1,27 @@
 class Solution {
 public:
-long long helper(vector<int>& coins, int i,vector<vector<long long>>& dp, int amount){
-        if(i==0){
-          if(amount%coins[i]==0) return amount/coins[i];
-          return INT_MAX; 
-        } 
-        if(dp[i][amount]!=-1) return dp[i][amount];
-        long long nottake= helper(coins, i-1, dp, amount);
-        long long take=INT_MAX;
-        if(coins[i]<=amount) take=1+helper(coins, i, dp, amount-coins[i]);
-        return dp[i][amount]=min(take,nottake);
+long long helper(vector<int>& coins,vector<vector<long long>>& dp, int amount){
+        for(int i=0; i<=amount; i++){
+            if(i%coins[0]==0) dp[0][i]=i/coins[0];
+            else dp[0][i] = LLONG_MAX;
+        }
+        for(int i=1; i<coins.size(); i++){
+            for(int j=0; j<=amount; j++){
+                long long nottake=dp[i-1][j];
+                long long take =LLONG_MAX;
+                if(coins[i]<=j && dp[i][j - coins[i]] != LLONG_MAX) take=1+dp[i][j-coins[i]];
+                dp[i][j]=min(take,nottake);
+            }
+        }
+        return dp[coins.size()-1][amount];
+
+
     }
     int coinChange(vector<int>& coins, int amount) {
-        long long n=coins.size();
+        int n=coins.size();
         vector<vector<long long>> dp(n, vector<long long>(amount+1, -1));
-        long long ans=helper(coins, n-1, dp, amount);
-        if(ans==INT_MAX) return -1;
+        long long ans=helper(coins, dp, amount);
+        if (ans >= LLONG_MAX) return -1;
         else return ans;
     }
 
