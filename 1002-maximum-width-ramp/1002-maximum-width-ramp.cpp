790862932e537b6c73e49f499231449ebe725e7c
Anukrati Chaturvedi <146018357+anukraticodes@users.noreply.github.com>
2024-10-10 12:48:37 +0530

    Time: 59 ms (31.45%), Space: 47.9 MB (11.4%) - LeetHub

--- 1002-maximum-width-ramp/1002-maximum-width-ramp.cpp
+++ 1002-maximum-width-ramp/1002-maximum-width-ramp.cpp
@@ -6,14 +6,14 @@ public:
 
     
     for(int i = 0; i < nums.size(); i++) {
-        if(st.empty() || nums[st.top().second] > nums[i]) {
+        if(st.empty() || st.top().first > nums[i]) {
             st.push({nums[i], i});
         }
     }
 
    
     for(int i = nums.size() - 1; i >= 0; i--) {
-        while(!st.empty() && nums[st.top().second] <= nums[i]) {
+        while(!st.empty() && st.top().first <= nums[i]) {
             ans = max(ans, i - st.top().second);
             st.pop();
         }
