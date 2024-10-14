207aec310b960ec7537662a9b05dc2b9e22e16b0
Anukrati Chaturvedi <146018357+anukraticodes@users.noreply.github.com>
2024-10-14 18:47:17 +0530

    Time: 36 ms (27.83%), Space: 26 MB (31.59%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
@@ -1,24 +1,26 @@
 class Solution {
 public:
-    int longestCommonSubsequence(string t1, string t2) {
+     int longestCommonSubsequence(string t1, string t2) {
         int n = t1.size(), m = t2.size();
-        // Create a (n+1) x (m+1) dp table initialized to 0
-        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+        vector<vector<int>> dp(n, vector<int>(m, 0));
 
         // Fill the dp table
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
-                if (t1[i - 1] == t2[j - 1]) {
-                    // If characters match, take the diagonal value + 1
-                    dp[i][j] = dp[i - 1][j - 1] + 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (t1[i] == t2[j]) {
+                    if (i > 0 && j > 0) {
+                        dp[i][j] = dp[i - 1][j - 1] + 1;
+                    } else {
+                        dp[i][j] = 1;  // Handle the first row/column
+                    }
                 } else {
-                    // If not, take the maximum from top or left cell
-                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                    int top = (i > 0) ? dp[i - 1][j] : 0;
+                    int left = (j > 0) ? dp[i][j - 1] : 0;
+                    dp[i][j] = max(top, left);
                 }
             }
         }
 
-        // The answer is in the bottom-right corner of the dp table
-        return dp[n][m];
+        return dp[n - 1][m - 1];
     }
 };
