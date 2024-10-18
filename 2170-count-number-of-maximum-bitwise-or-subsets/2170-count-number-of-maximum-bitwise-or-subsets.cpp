83068f35430b14767c3673e6b3b535b46d074d80
Anukrati Chaturvedi <146018357+anukraticodes@users.noreply.github.com>
2024-10-18 12:21:15 +0530

    Time: 10 ms (83.65%), Space: 10.1 MB (82.14%) - LeetHub

--- 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.cpp
+++ 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.cpp
@@ -8,29 +8,18 @@ public:
             int take=(nums[i]|dp[i-1]);
             dp[i]=max(take, dp[i-1]);
         }
-        int maxi=dp[n-1];
+       
         int cnt=0;
-        vector<vector<int>> result;
-        vector<int> v;
-        helper(0,v, nums, n, result);
-        for(int i=0; i<result.size(); i++){
-            int val=0;
-            for(int j=0; j<result[i].size(); j++){
-                val|=result[i][j];
-            }
-            if(val==maxi) cnt++;
-        }
+        
+        helper(nums, 0, 0, dp[n-1], cnt);
         return cnt;        
     }
 
-    void helper(int i,vector<int>& v, vector<int>& nums, int n, vector<vector<int>>& result){
-        if(i==n){
-            result.push_back(v);
-            return;
-        }
-        v.push_back(nums[i]);
-        helper(i+1, v, nums, n, result);
-        v.pop_back();
-        helper(i+1,v,nums,n, result);        
+    void helper(const vector<int>& nums, int i, int curr, int maxi, int& cnt){
+        if(curr==maxi) cnt++;
+
+        for(int j=i; j<nums.size(); j++){
+            helper(nums, j+1, curr|nums[j], maxi, cnt);
+        }      
     }
 };
