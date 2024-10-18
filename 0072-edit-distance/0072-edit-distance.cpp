fdd6331ca9a85a99795aadc41602007a4251224d
Anukrati Chaturvedi <146018357+anukraticodes@users.noreply.github.com>
2024-10-18 14:03:15 +0530

    Time: 11 ms (37.07%), Space: 13 MB (27.73%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -2,26 +2,28 @@ class Solution {
 public:
     int minDistance(string s, string t) {
     int n = s.size(), m = t.size();
-    vector<vector<int>> dp(n, vector<int>(m, -1));
-    return helper(s, t, n - 1, m - 1, dp);
+    vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+    return helper(s, t, n , m , dp);
 }
 
-int helper(const string& s, const string& t, int i, int j, vector<vector<int>>& dp) {
-   
-    if (i < 0) return j + 1;  
-    if (j < 0) return i + 1;  
-
-   
-    if (dp[i][j] != -1) return dp[i][j];
-
-    if (s[i] == t[j]) {
-        return dp[i][j] = helper(s, t, i - 1, j - 1, dp);
+int helper(const string& s, const string& t, int n, int m, vector<vector<int>>& dp) {
+       // if (i < 0) return j + 1;  
+    // if (j < 0) return i + 1;  
+    // if (dp[i][j] != -1) return dp[i][j];
+    for(int i=0; i<=n; i++){
+        dp[i][0]=i;
     }
-
-    int substitute = helper(s, t, i - 1, j - 1, dp);  
-    int deleteChar = helper(s, t, i - 1, j, dp);       
-    int insertChar = helper(s, t, i, j - 1, dp);      
-
-    return dp[i][j] = 1 + min({substitute, deleteChar, insertChar});
+    for(int j=0; j<=m; j++){
+        dp[0][j]=j;
+    }
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=m; j++){
+            if(s[i-1]==t[j-1]) dp[i][j]=dp[i-1][j-1];
+            else{
+                dp[i][j]=1+min({dp[i-1][j-1], dp[i-1][j], dp[i][j-1]});
+            }
+        }
+    }
+    return dp[n][m];
 }
 };
