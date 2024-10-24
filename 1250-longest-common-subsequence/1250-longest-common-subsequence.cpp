bc33843069f8a13502cbe3322fe6a8c316e0ef84
Anukrati Chaturvedi <146018357+anukraticodes@users.noreply.github.com>
2024-10-24 11:06:40 +0530

    Time: 27 ms (48.79%), Space: 26.1 MB (19.97%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
@@ -7,18 +7,18 @@ public:
     }
 
     int helper(string t1, string t2, vector<vector<int>>& dp){
-      for(int i=0; i<=t1.size(); i++){
-        dp[i][0]=0;
-       }
-       for(int i=0; i<=t2.size(); i++){
-        dp[0][i]=0;
-       }
+    //   for(int i=1; i<=t1.size(); i++){
+    //     if(t1[i-1]==t2[0]) dp[i-1][0]=1;
+    //    }
+    //    for(int i=1; i<=t2.size(); i++){
+    //     if(t1[0]==t2[i-1]) dp[0][i-1]=1;
+    //    }
         for(int i=1; i<=t1.size(); i++){
             for(int j=1; j<=t2.size(); j++){
-                
-                if(t1[i-1]==t2[j-1]) dp[i][j]=1+dp[i-1][j-1];
+               
+                if(t1[i-1]==t2[j-1]) dp[i][j]=dp[i-1][j-1]+1;
                 else dp[i][j]=max(dp[i-1][j], dp[i][j-1]);
-                
+               
             }
         }
         return dp[t1.size()][t2.size()];
