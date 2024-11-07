1fcd3dddcc57e77e41b25445039ebdf799581f9a
Anukrati Chaturvedi <146018357+anukraticodes@users.noreply.github.com>
2024-11-07 15:18:11 +0530

    Time: 27 ms (73.13%), Space: 60.2 MB (32.98%) - LeetHub

--- 2356-largest-combination-with-bitwise-and-greater-than-zero/2356-largest-combination-with-bitwise-and-greater-than-zero.cpp
+++ 2356-largest-combination-with-bitwise-and-greater-than-zero/2356-largest-combination-with-bitwise-and-greater-than-zero.cpp
@@ -3,7 +3,7 @@ public:
     int largestCombination(vector<int>& c) {
         int n=c.size();
         int cnt=INT_MIN;
-        for(int i=0; i<32; i++){
+        for(int i=0; i<26; i++){
             int ans=0;
             for(auto ca:c){
                 if(ca&(1<<i)) ans++;
