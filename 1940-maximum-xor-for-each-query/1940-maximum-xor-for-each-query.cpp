5898e79c295f52001799d61629f7249248dc7fc5
Anukrati Chaturvedi <146018357+anukraticodes@users.noreply.github.com>
2024-11-08 17:20:32 +0530

    Time: 0 ms (100%), Space: 99.8 MB (27.33%) - LeetHub

--- 1940-maximum-xor-for-each-query/1940-maximum-xor-for-each-query.cpp
+++ 1940-maximum-xor-for-each-query/1940-maximum-xor-for-each-query.cpp
@@ -4,10 +4,11 @@ public:
     int n = nums.size();
     vector<int> ans;
     
-    int x ;  
+    int x=nums[0];
+    ans.push_back(x xor ((1<<m)-1));  
 
-    for (int i = 0; i < n; i++) {
-         i==0?x=nums[i]:x=x xor nums[i];  // XOR the current number with the cumulative XOR
+    for (int i = 1; i < n; i++) {
+        x=x xor nums[i];  // XOR the current number with the cumulative XOR
         ans.push_back(x xor ((1<<m)-1));
         
     }
