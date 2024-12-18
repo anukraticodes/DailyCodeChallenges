abf53c3b3cf01f9fa5266d0ad10bf1a2bc3a37c3
Anukrati Chaturvedi <146018357+anukraticodes@users.noreply.github.com>
2024-12-18 20:15:05 +0530

    Time: 0 ms (100%), Space: 14.3 MB (9.86%) - LeetHub

--- 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.cpp
+++ 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.cpp
@@ -1,20 +1,16 @@
 class Solution {
 public:
     vector<int> finalPrices(vector<int>& p) {
-        vector<int> answer;
+        stack<int> st;
         int n=p.size();
-        for(int i=0; i<n-1; i++){
-            for(int j=i+1; j<n; j++){
-                if(p[j]<=p[i]){
-                    answer.push_back(p[i]-p[j]);
-                    break;
-                }
-                else if(j==n-1) answer.push_back(p[i]);
+        for(int i=0; i<n; i++){
+            while(!st.empty() && p[i]<=p[st.top()]){
+                int ind=st.top();
+                st.pop();
+                p[ind]-=p[i];
             }
-            
+            st.push(i);
         }
-        answer.push_back(p[n-1]);
-        return answer;
-
+        return p;
     }
 };
