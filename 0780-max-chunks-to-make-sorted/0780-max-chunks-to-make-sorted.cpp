38c800595cd1300d8e6d280bebaeea07af1e8d90
Anukrati Chaturvedi <146018357+anukraticodes@users.noreply.github.com>
2024-12-19 14:55:25 +0530

    Time: 0 ms (100%), Space: 9.2 MB (20.72%) - LeetHub

--- 0780-max-chunks-to-make-sorted/0780-max-chunks-to-make-sorted.cpp
+++ 0780-max-chunks-to-make-sorted/0780-max-chunks-to-make-sorted.cpp
@@ -2,13 +2,16 @@ class Solution {
 public:
     int maxChunksToSorted(vector<int>& arr) {
         int n=arr.size();
-        int ans=0;
-        int sum=0;
+        stack<int> st;
         for(int i=0; i<n; i++){
-            sum+=arr[i];
-            if(sum==(i*(i+1))/2) ans++;
+            if(st.empty()||st.top()<arr[i]) st.push(arr[i]);
+            else{
+                int maxe=st.top();
+                while(!st.empty()&& st.top()>arr[i]) st.pop();
+            
+            st.push(maxe);
+            }
         }
-
-        return ans;
+        return st.size();
     }
 };
