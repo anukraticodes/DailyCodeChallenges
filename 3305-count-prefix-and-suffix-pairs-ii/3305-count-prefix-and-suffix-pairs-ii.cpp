5f6738bdd7ebd51e0a095687e1808d84a310fab8
Anukrati Chaturvedi <146018357+anukraticodes@users.noreply.github.com>
2025-01-08 20:34:50 +0530

    Time: 44 ms (91.41%), Space: 67.8 MB (95.09%) - LeetHub

--- 3305-count-prefix-and-suffix-pairs-ii/3305-count-prefix-and-suffix-pairs-ii.cpp
+++ 3305-count-prefix-and-suffix-pairs-ii/3305-count-prefix-and-suffix-pairs-ii.cpp
@@ -2,10 +2,12 @@ class Solution {
 public:
     long long countPrefixSuffixPairs(vector<string>& words) {
         unordered_map<int64_t, int> freq;
+        
         int64_t ans=0, mod=(int64_t)1e17+3;
         for(auto& w : words){
+            int n=w.size();
             int64_t l_hash=0, r_hash=0, pow=1;
-            for(int n=w.size(), i=1; i<=n; i++){
+            for(int i=1; i<=n; i++){
                 l_hash=(l_hash*27LL+(1LL+w[i-1]-'a'))% mod;
                 r_hash = ((1LL + w[n - i] - 'a') * pow % mod + r_hash) % mod;
                 pow=pow*27%mod;
