809a5f214e58b06543a5e64ed42cefb3e00313fe
Anukrati Chaturvedi <146018357+anukraticodes@users.noreply.github.com>
2025-01-08 17:41:05 +0530

    Time: 10 ms (19.28%), Space: 45.9 MB (5.08%) - LeetHub

--- 3309-count-prefix-and-suffix-pairs-i/3309-count-prefix-and-suffix-pairs-i.cpp
+++ 3309-count-prefix-and-suffix-pairs-i/3309-count-prefix-and-suffix-pairs-i.cpp
@@ -1,25 +1,67 @@
+class Node{
+    public:
+    
+    Node* links[26]={};
+    bool contains(char c){
+        return links[c-'a']!=NULL;
+    }
+
+    void put(char c, Node* node){
+        links[c-'a']=node;
+    }
+
+    Node* next(char c){
+        return links[c-'a'];
+    }
+
+};
+
+class Trie{
+    public:
+    Node* root;
+
+    Trie(){ root=new Node();}
+
+    void insert(string& word){
+        Node* node=root;
+     for(char c: word){
+    if(!node->contains(c)) node->put(c,new Node());
+        node=node->next(c);
+}    }
+
+bool find(string& prefix){
+    Node* node=root;
+    for(char c:prefix){
+        if(!node->contains(c)) return 0;
+        node=node->next(c);
+    }
+    return 1;
+}
+};
+
 class Solution {
 public:
     int countPrefixSuffixPairs(vector<string>& words) {
+        int n=words.size();
         int cnt=0;
-        for(int i=0; i<words.size(); i++){
-            for(int j=i+1; j<words.size(); j++){
-                if(isreal(words[i], words[j])) cnt++;
+        for(int i=0; i<n; i++){
+            Trie prefixTrie, suffixTrie;
+            prefixTrie.insert(words[i]);
+            string revWord=words[i];
+            reverse(revWord.begin(), revWord.end());
+            suffixTrie.insert(revWord);
+
+            for(int j=0; j<i; j++){
+                if(words[j].size()>words[i].size()) continue;
+                string prefixWord=words[j];
+                string revPrefixWord=prefixWord;
+                reverse(revPrefixWord.begin(), revPrefixWord.end());
+                if (prefixTrie.find(prefixWord) &&
+                    suffixTrie.find(revPrefixWord)) {
+                    cnt++;
+                }
             }
         }
         return cnt;
     }
-
-  bool isreal(string s1, string s2)
-{
-    int n=s1.size();
-    int n1=s2.size();
-    if(n1<n) return 0;
-    string str1="", str2="";
-    for(int i=0; i<n;i++){
-    str1+=s2[i];
-    str2+=s2[n1-1-i];
-    }
-    reverse(str2.begin(), str2.end());
-    return s1==str1 && s1==str2;
-}};
+};
