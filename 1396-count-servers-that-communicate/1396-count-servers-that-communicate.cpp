88c6dba8e05d90d741148e20f2fec798b32c4ae1
Anukrati Chaturvedi <146018357+anukraticodes@users.noreply.github.com>
2025-01-23 15:45:46 +0530

    Time: 6 ms (33.6%), Space: 26.5 MB (81.36%) - LeetHub

--- 1396-count-servers-that-communicate/1396-count-servers-that-communicate.cpp
+++ 1396-count-servers-that-communicate/1396-count-servers-that-communicate.cpp
@@ -1,31 +1,5 @@
 class Solution {
 public:
-       struct DSU{
-        vector<int> parent, rank;
-        DSU(int n){
-            parent.resize(n);
-            rank.resize(n, 0);
-            for(int i=0; i<n; i++) parent[i]=i;
-        }
-
-        int find(int node){
-            if(parent[node]!=node) parent[node]=find(parent[node]);
-            return parent[node];
-        }
-
-        void unite(int u, int v){
-            int pu=find(u), pv=find(v);
-            if(pu!=pv){
-                if(rank[pu]>rank[pv]) parent[pv]=pu;
-                else if(rank[pv]>rank[pu]) parent[pu]=pv;
-                else{
-                    parent[pu]=pv;
-                    rank[pv]++;
-                }
-            }
-        }
-    };
-
     int countServers(vector<vector<int>>& g) {
         vector<pair<int, int>> grid;
         for(int i=0; i<g.size(); i++){
@@ -33,24 +7,19 @@ public:
                 if(g[i][j]==1) grid.push_back({i, j});
             }
         }
-        DSU dsu(grid.size());
+        
         int n=grid.size();
+        int ans=0;
         for(int i=0; i<n; i++){
-            for(int j=i+1; j<n; j++){
-                if(grid[i].first==grid[j].first || grid[i].second==grid[j].second)
-                dsu.unite(i,j);
+            for(int j=0; j<n; j++){
+                if((grid[i].first==grid[j].first || grid[i].second==grid[j].second)&&(i!=j)){
+                    ans++;
+                    break;
+                }
+                
             }
         }
-        
-        unordered_map<int, vector<int>> mp;
-        for(int i=0; i<n; i++){
-            int x=dsu.find(i);
-            mp[x].push_back(i);
-        }
-        int cnt=0;
-        for(auto it:mp){
-            if(it.second.size()>1) cnt+=it.second.size();
-        }
-        return cnt;
+       
+        return ans;
     }
 };
