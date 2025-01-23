d6167f281b174e74556e94d978f7b0b57a94376e
Anukrati Chaturvedi <146018357+anukraticodes@users.noreply.github.com>
2025-01-23 16:23:27 +0530

    Time: 0 ms (100%), Space: 16.9 MB (96.86%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
+
+    bool dfs(int node, vector<bool>& vis, bool c, vector<bool>& colour, vector<vector<int>>& graph){
+        vis[node]=1;
+        colour[node]=c;
+        for(auto nbr:graph[node]){
+            if(!vis[nbr]){
+                if(!dfs(nbr, vis, !c, colour, graph)) return false;
+            }
+            if(colour[nbr]==c) return false;
+        }
+        return true;
+    }
     bool isBipartite(vector<vector<int>>& graph) {
         int n=graph.size();
-        queue<pair<int, bool>> q;
         vector<bool> vis(n, 0);
-        vector<bool> color(n);
+        vector<bool> colour(n);
         for(int i=0; i<n; i++){
-          if(vis[i]==0) q.push({i,0});
-        
-        while(!q.empty()){
-            auto[node, c]=q.front();
-            q.pop();
-            vis[node]=1;
-            color[node]=c;
-            for(auto nbr:graph[node]){
-                 if(!vis[nbr]){
-                    q.push({nbr, !c});
-                }
-                else if(color[nbr]==c) return false;
+            if(!vis[i]){
+                if(!dfs(i, vis, 0, colour, graph)) return false;
             }
-        }}
+        }
         return true;
     }
 };
