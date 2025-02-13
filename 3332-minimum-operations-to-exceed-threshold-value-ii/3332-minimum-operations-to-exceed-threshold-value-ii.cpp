bdc1e60c21c59cebb8978195578540b15f4556d2
Anukrati Chaturvedi <146018357+anukraticodes@users.noreply.github.com>
2025-02-13 15:47:09 +0530

    Time: 91 ms (81.17%), Space: 91.5 MB (74%) - LeetHub

--- 3332-minimum-operations-to-exceed-threshold-value-ii/3332-minimum-operations-to-exceed-threshold-value-ii.cpp
+++ 3332-minimum-operations-to-exceed-threshold-value-ii/3332-minimum-operations-to-exceed-threshold-value-ii.cpp
@@ -1,24 +1,19 @@
 class Solution {
 public:
-
-    void findmin(multiset<long long>& arr){
-        // long long x=INT_MAX, y=INT_MAX;
-         auto it = arr.begin();
-        long long x = *it; ++it;
-        long long y = *it;
-        
-        arr.erase(arr.begin());
-        arr.erase(arr.begin()); 
-        arr.insert(2 * x + y);      
-    }
     int minOperations(vector<int>& nums, int k) {
-        multiset<long long> n(nums.begin(), nums.end());
-        int cnt=0;
-        while (n.size() >= 2 && *n.begin() < k) {
-            cnt++;
-            findmin(n);  // Perform the operation
+        priority_queue<long, vector<long>, greater<long>> min_heap(nums.begin(),
+                                                                   nums.end());
+        int num_operations = 0;
+
+        while (min_heap.top() < k) {
+            long x = min_heap.top();
+            min_heap.pop();
+            long y = min_heap.top();
+            min_heap.pop();
+            min_heap.push(min(x, y) * 2 + max(x, y));
+
+            num_operations++;
         }
-        
-        return cnt;
+        return num_operations;
     }
 };
