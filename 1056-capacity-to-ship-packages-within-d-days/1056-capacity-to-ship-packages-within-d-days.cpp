d0a32ac4e12f211a256aee6a636f15ce55115f9b
Anukrati Chaturvedi <146018357+anukraticodes@users.noreply.github.com>
2025-03-22 15:16:32 +0530

    Time: 7 ms (92.55%), Space: 35 MB (35.68%) - LeetHub

---
+++ 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+
+    bool helper(vector<int>& w, int days, int mid){
+        int total=1, cnt=0;      
+        for(int i=0; i<w.size(); i++){
+            if(cnt+w[i]>mid){
+                total++;cnt=0;
+            }
+            cnt+=w[i];
+        }
+        return total<=days;
+    }
+
+    int shipWithinDays(vector<int>& w, int days) {
+        int l=*max_element(w.begin(), w.end());
+        int r=accumulate(w.begin(), w.end(), 0);
+        while(l<r){
+            int mid=l+(r-l)/2;
+            if(helper(w, days, mid))r=mid;
+            else l=mid+1;
+        }
+        return l;
+    }
+};
