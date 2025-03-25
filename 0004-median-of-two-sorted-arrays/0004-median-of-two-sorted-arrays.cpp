415c6672b8adc461863d1335637cad218cb5dd29
Anukrati Chaturvedi <146018357+anukraticodes@users.noreply.github.com>
2025-03-25 16:12:19 +0530

    Time: 0 ms (100%), Space: 95.1 MB (63.65%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -19,7 +19,7 @@ public:
             if(n%2==1) return max(l1, l2);
             else return ((double) (max(l1, l2)+min(r1, r2)))/2.0;
           }
-          else if(l1>l2) high=mid1-1;
+          else if(l1>r2) high=mid1-1;
           else low=mid1+1;
         }
         return 0;
