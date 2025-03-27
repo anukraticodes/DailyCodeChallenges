5ff657688a80a8d346624c97de0479e4a1fccde6
Anukrati Chaturvedi <146018357+anukraticodes@users.noreply.github.com>
2025-03-27 10:15:39 +0530

    Time: 0 ms (100%), Space: 89.9 MB (70.9%) - LeetHub

--- 2888-minimum-index-of-a-valid-split/2888-minimum-index-of-a-valid-split.cpp
+++ 2888-minimum-index-of-a-valid-split/2888-minimum-index-of-a-valid-split.cpp
@@ -1,17 +1,28 @@
 class Solution {
 public:
     int minimumIndex(vector<int>& nums) {
-        int dom=-1, domcnt=-1;
-        unordered_map<int, int> mp;
-        for(int i=0; i<nums.size(); i++){
-            mp[nums[i]]++;
-        }
-        for(auto it:mp){
-            if(it.second>nums.size()/2){
-                dom=it.first;
-                domcnt=it.second;
+        int dom=nums[0], domcnt=0, count=0;
+        for(auto& num:nums){
+            if(num==dom) count++;
+            else count--;
+            if(count==0){
+                dom=num;
+                count=1;
             }
         }
+        for(auto& num:nums){
+            if(num==dom) domcnt++;
+        }
+        // unordered_map<int, int> mp;
+        // for(int i=0; i<nums.size(); i++){
+        //     mp[nums[i]]++;
+        // }
+        // for(auto it:mp){
+        //     if(it.second>nums.size()/2){
+        //         dom=it.first;
+        //         domcnt=it.second;
+        //     }
+        // }
         int valid=-1, cnt=0;
         for(int i=0; i<nums.size(); i++){
            if(nums[i]==dom) cnt++;
