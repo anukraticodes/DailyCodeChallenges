d2ec8c30fabb8d143b895494485f144285a1e1ae
Anukrati Chaturvedi <146018357+anukraticodes@users.noreply.github.com>
2025-03-28 16:42:03 +0530

    Time: 0 ms (100%), Space: 15.7 MB (36.21%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -5,12 +5,13 @@ public:
         return ans;
     }
 
-    vector<string> helper(int openP, int closeP, string s, int n, vector<string>& ans){
+    void helper(int openP, int closeP, string s, int n, vector<string>& ans){
         if(openP==closeP && openP+closeP==2*n){
             ans.push_back(s);
+            return;
         }
         if(openP<n) helper(openP+1, closeP, s+"(", n, ans);
         if(closeP<openP) helper(openP, closeP+1, s+")", n, ans);
-        return ans;
+        // return ans;
     }
 };
