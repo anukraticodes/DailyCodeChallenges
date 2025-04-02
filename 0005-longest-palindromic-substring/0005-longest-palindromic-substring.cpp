c5f0d9ed263aea81535bad855d25c32458ee66d4
Anukrati Chaturvedi <146018357+anukraticodes@users.noreply.github.com>
2025-04-02 18:16:20 +0530

    Time: 68 ms (56.52%), Space: 26.2 MB (36.46%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,29 +1,24 @@
 class Solution {
 public:
-    std::string longestPalindrome(std::string s) {
-        if (s.length() <= 1) {
-            return s;
-        }
-        
-        int max_len = 1;
-        int start = 0;
-        int end = 0;
-        std::vector<std::vector<bool>> dp(s.length(), std::vector<bool>(s.length(), false));
-        
-        for (int i = 0; i < s.length(); ++i) {
-            dp[i][i] = true;
-            for (int j = 0; j < i; ++j) {
-                if (s[j] == s[i] && (i - j <= 2 || dp[j + 1][i - 1])) {
-                    dp[j][i] = true;
-                    if (i - j + 1 > max_len) {
-                        max_len = i - j + 1;
-                        start = j;
-                        end = i;
+    string longestPalindrome(string s) {
+        if(s.size()<=1) return s;
+        int start=0, end=0;
+        int maxl=1;
+
+        vector<vector<bool>> dp(s.size(), vector<bool>(s.size(), 0));
+        for(int i=0; i<s.size(); i++){
+            dp[i][i]=1;
+            for(int j=0; j<i; j++){
+                if(s[i]==s[j] && (i-j<=2 || dp[j+1][i-1])){
+                    dp[j][i]=1;
+                    if(i-j+1>=maxl){
+                        maxl=i-j+1;
+                        start=j;
+                        end=i;
                     }
                 }
             }
         }
-        
-        return s.substr(start, end - start + 1);
+        return s.substr(start, maxl);
     }
 };
