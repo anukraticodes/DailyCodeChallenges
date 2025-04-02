b2b499290ea281e895a63dfa40e940bb5f3de499
Anukrati Chaturvedi <146018357+anukraticodes@users.noreply.github.com>
2025-04-02 18:10:07 +0530

    Time: 88 ms (43.03%), Space: 26.2 MB (36.46%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,29 +1,29 @@
 class Solution {
 public:
-    string longestPalindrome(string s) {
-        if (s.empty()) {
-            return "";
+    std::string longestPalindrome(std::string s) {
+        if (s.length() <= 1) {
+            return s;
         }
-        int start=0, end=0;
-
-        for(int i=0; i<s.size(); i++){
-            int odd=helper(i, i, s);
-            int even=helper(i, i+1, s);
-            int maxl=max(odd, even);
-
-            if(maxl>end-start){
-                start=i-(maxl-1)/2;
-                end=i+maxl/2;
+        
+        int max_len = 1;
+        int start = 0;
+        int end = 0;
+        std::vector<std::vector<bool>> dp(s.length(), std::vector<bool>(s.length(), false));
+        
+        for (int i = 0; i < s.length(); ++i) {
+            dp[i][i] = true;
+            for (int j = 0; j < i; ++j) {
+                if (s[j] == s[i] && (i - j <= 2 || dp[j + 1][i - 1])) {
+                    dp[j][i] = true;
+                    if (i - j + 1 > max_len) {
+                        max_len = i - j + 1;
+                        start = j;
+                        end = i;
+                    }
+                }
             }
         }
-        return s.substr(start, end-start+1);
-    }
-
-    int helper(int i, int j, string s){
-        while(i>=0 && j<s.size() && s[i]==s[j]){
-            i--;
-            j++;
-        }
-        return j-i-1;
+        
+        return s.substr(start, end - start + 1);
     }
 };
