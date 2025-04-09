16951dd23a8a368f550b6b5b5e0aba42ec506209
Anukrati Chaturvedi <146018357+anukraticodes@users.noreply.github.com>
2025-04-09 19:45:06 +0530

    Time: 7 ms (89.94%), Space: 32.4 MB (57.55%) - LeetHub

--- 3621-minimum-operations-to-make-array-values-equal-to-k/3621-minimum-operations-to-make-array-values-equal-to-k.cpp
+++ 3621-minimum-operations-to-make-array-values-equal-to-k/3621-minimum-operations-to-make-array-values-equal-to-k.cpp
@@ -1,31 +1,10 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums, int k) {
-        int cnt=0;
-        map<int, int> mp;
-        if(nums.size()==1){
-            if(nums[0]<k) return -1;
-            if(nums[0]==k) return 0;
-            return 1;
-        }
-        for(auto num:nums){
-            mp[num]++;
-        }
-        bool flag=0;
-        if(mp.find(k)!=mp.end()) flag=1;
-        while(mp.size()>1){
-            auto it = mp.rbegin();
-            if(it->first <=k) return -1;
-            int largest=it->first, cnt1=it->second;
-            it++;
-            int largest2=it->first, cnt2=it->second;
-            mp[largest2]+=cnt1;
-            mp.erase(largest);
-            cnt++;
-        }
-        auto it=mp.rbegin();
-        if(it->first <k) return -1;
-
-        return flag==1?cnt:cnt+1;
+        set<int> st(nums.begin(), nums.end());
+        int mini=*st.begin();
+        if(mini<k) return -1;
+        int ans=st.size();
+        return st.count(k)? ans-1:ans;
     }
 };
