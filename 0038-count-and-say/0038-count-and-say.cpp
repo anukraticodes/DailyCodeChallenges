c3e2260c6649524faa9bc8e4077f5c185753483a
Anukrati Chaturvedi <146018357+anukraticodes@users.noreply.github.com>
2025-04-18 10:44:48 +0530

    Time: 3 ms (83.88%), Space: 10.1 MB (45.09%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.cpp
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -2,17 +2,7 @@ class Solution {
 public:
     string countAndSay(int n) {
         if(n==1) return "1";
-        string s="1";
-        string ans;
-        for(int i=0; i<n-1; i++){
-           ans=rle(s);
-           s=ans;
-        }
-        return ans;
-        
-    }
-
-    string rle(string& s){
+        string s = countAndSay(n-1);
         int cnt=0; char ch=s[0];
         string s1="";
         for(auto c:s){
@@ -26,5 +16,22 @@ public:
         s1+=to_string(cnt);
         s1+=ch;
         return s1;
+        
     }
+
+    // string rle(string& s){
+    //     int cnt=0; char ch=s[0];
+    //     string s1="";
+    //     for(auto c:s){
+    //        if(c==ch) cnt++;
+    //        else {s1+=to_string(cnt);
+    //        s1+=ch;
+    //        ch=c;
+    //        cnt=1;
+    //        }
+    //     }
+    //     s1+=to_string(cnt);
+    //     s1+=ch;
+    //     return s1;
+    // }
 };
