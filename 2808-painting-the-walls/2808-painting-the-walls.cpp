eb7734bb6cf527c15ad00422c81c12ede4006c75
Anukrati Chaturvedi <146018357+anukraticodes@users.noreply.github.com>
2025-05-12 16:55:29 +0530

    Time: 15 ms (94.85%), Space: 94.4 MB (98.97%) - LeetHub

--- 2808-painting-the-walls/2808-painting-the-walls.cpp
+++ 2808-painting-the-walls/2808-painting-the-walls.cpp
@@ -1,21 +1,12 @@
+int dp[501];
 class Solution {
 public:
-    int paintWalls(vector<int>& cost, vector<int>& time) {
-        int n = cost.size();
-        vector<vector<int>> dp(n + 1, vector(n + 1, 0));
-        
-        for (int i = 1; i <= n; i++) {
-            dp[n][i] = 1e9;
-        }
-        
-        for (int i = n - 1; i >= 0; i--) {
-            for (int remain = 1; remain <= n; remain++) {
-                int paint = cost[i] + dp[i + 1][max(0, remain - 1 - time[i])];
-                int dontPaint = dp[i + 1][remain];
-                dp[i][remain] = min(paint, dontPaint);
-            }
-        }
-        
-        return dp[0][n];
-    }
+int paintWalls(vector<int>& cost, vector<int>& time) {
+    fill_n(begin(dp), cost.size() + 1, 1e9);
+    dp[0] = 0;
+    for (int i = 0; i < cost.size(); ++i)
+        for (int j = cost.size(); j > 0; --j)
+            dp[j] = min(dp[j], dp[max(0, j - time[i] - 1)] + cost[i]);
+    return dp[cost.size()];
+}
 };
