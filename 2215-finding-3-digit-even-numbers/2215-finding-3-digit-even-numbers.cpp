4331c9d61a3bc74d1651a3122b2648fcb5e742ba
Anukrati Chaturvedi <146018357+anukraticodes@users.noreply.github.com>
2025-05-12 17:20:05 +0530

    Time: 0 ms (100%), Space: 12.5 MB (87.12%) - LeetHub

--- 2215-finding-3-digit-even-numbers/2215-finding-3-digit-even-numbers.cpp
+++ 2215-finding-3-digit-even-numbers/2215-finding-3-digit-even-numbers.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
     vector<int> findEvenNumbers(vector<int>& digits) {
-        set<int> st;
-        for(int i=0; i<digits.size(); i++){
-            if(digits[i]==0) continue;
-            
-            for(int j=0; j<digits.size(); j++){
-                if(j == i) continue;
-                for(int k=0; k<digits.size(); k++){
-                    if(k == i || k == j) continue;
-                    int num = digits[i] * 100 + digits[j] * 10 + digits[k];
-                    if(num % 2 == 0) {
-                        st.insert(num);
-                    }
+        vector<int> cnt(10);
+        for(int i:digits) cnt[i]++;
+        vector<int> ans;
+        for(int i=1; i<10; i++){
+            if(cnt[i]==0) continue;
+            cnt[i]--;
+            for(int j=0; j<10; j++){
+                if(cnt[j]==0) continue;
+                cnt[j]--;
+                for(int k=0; k<10; k+=2){
+                    if(cnt[k]==0) continue;
+                    ans.push_back(i*100+j*10+k);
                 }
+                cnt[j]++;
             }
+            cnt[i]++;
         }
-        vector<int> ans(st.begin(), st.end());
         return ans;
     }
 };
