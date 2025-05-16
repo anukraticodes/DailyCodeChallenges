1e4dc45f83180a22a41f98127135a52dbc03665c
Anukrati Chaturvedi <146018357+anukraticodes@users.noreply.github.com>
2025-05-16 18:41:43 +0530

    Time: 0 ms (100%), Space: 13.5 MB (5.78%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.cpp
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.cpp
@@ -12,17 +12,17 @@
 class Solution {
 public:
 
-   void dfs(TreeNode* root, string current, vector<string>& result)
+   void dfs(TreeNode* root, string& current, vector<string>& result)
 {
     if (!root) return;
 
-    current += to_string(root->val); // change is local
-
+    current += to_string(root->val);
     if (!root->left && !root->right)
         result.push_back(current);
 
     dfs(root->left, current, result);
     dfs(root->right, current, result);
+    current.pop_back();
 }
 
     int sumNumbers(TreeNode* root) {
