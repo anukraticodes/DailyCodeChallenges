74d4afa5dba010783c7c7fe0532f0f3bcb14d269
Anukrati Chaturvedi <146018357+anukraticodes@users.noreply.github.com>
2025-05-23 15:22:37 +0530

    Time: 0 ms (100%), Space: 12.1 MB (29.58%) - LeetHub

--- 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.cpp
+++ 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.cpp
@@ -5,7 +5,7 @@ public:
         for(auto n:arr) mp[n]++;
         unordered_set<int> st;
         for(auto it:mp) st.insert(it.second);
-        unordered_set<int> st2(arr.begin(), arr.end());
-        return st.size()==st2.size();
+        // unordered_set<int> st2(arr.begin(), arr.end());
+        return st.size()==mp.size();
     }
 };
