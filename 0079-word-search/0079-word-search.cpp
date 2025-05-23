98ee188d4d20123f5e5ea2ac65aa80f14600013b
Anukrati Chaturvedi <146018357+anukraticodes@users.noreply.github.com>
2025-05-23 16:11:03 +0530

    Time: 761 ms (39.08%), Space: 11.5 MB (47.49%) - LeetHub

---
+++ 0079-word-search/0079-word-search.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool exist(vector<vector<char>>& board, string word) {
+        int m=board.size(), n=board[0].size();
+        function<bool(int, int, int)> backtrack=[&](int i, int j, int k){
+            if(k==word.length()) return 1;
+            if(i<0||i>=m||j<0||j>=n||board[i][j]!=word[k]) return 0;
+            char temp=board[i][j];
+            board[i][j]='\0';
+            if(backtrack(i+1, j, k+1)||backtrack(i-1, j, k+1)||backtrack(i, j+1, k+1)||backtrack(i, j-1, k+1)) return 1;
+            board[i][j]=temp;
+            return 0;
+        };
+        for(int i=0;i<m; i++){
+            for(int j=0; j<n; j++){
+                if(backtrack(i, j, 0)) return 1;
+            }
+        }
+        return 0;
+    }
+};
