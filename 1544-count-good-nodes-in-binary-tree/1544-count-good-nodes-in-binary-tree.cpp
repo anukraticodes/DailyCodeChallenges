546a6313e105bf4862479338afd8f1a253e9682d
Anukrati Chaturvedi <146018357+anukraticodes@users.noreply.github.com>
2025-05-30 10:10:40 +0530

    Time: 94 ms (74.53%), Space: 88.4 MB (61.41%) - LeetHub

--- 1544-count-good-nodes-in-binary-tree/1544-count-good-nodes-in-binary-tree.cpp
+++ 1544-count-good-nodes-in-binary-tree/1544-count-good-nodes-in-binary-tree.cpp
@@ -12,23 +12,14 @@
 class Solution {
 public:
 
-    void helper(TreeNode* root, int& ans, vector<int>& curr){
-        if(!root){
-            return;
-        }
-        if(!curr.empty() && *max_element(curr.begin(), curr.end())<=root->val){
-            ans++;
-        }
-        curr.push_back(root->val);
-        if(root->left) helper(root->left, ans, curr);
-        if(root->right) helper(root->right, ans, curr);
-        curr.pop_back();
+    int helper(TreeNode* node, int val){
+        if(!node) return 0;
+        int cnt=(node->val>=val)?1:0;
+        val=max(val, node->val);
+        return cnt+helper(node->left, val)+helper(node->right, val);
     }
 
     int goodNodes(TreeNode* root) {
-        int ans=1;
-        vector<int> curr;
-        helper(root, ans, curr);
-        return ans;
+        return helper(root, root->val);
     }
 };
