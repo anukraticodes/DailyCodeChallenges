cddce97bfe1c1351b458774f57f981f37788dbf1
Anukrati Chaturvedi <146018357+anukraticodes@users.noreply.github.com>
2025-05-30 09:23:39 +0530

    Time: 3 ms (32.78%), Space: 11.6 MB (17.82%) - LeetHub

---
+++ 1022-unique-paths-iii/1022-unique-paths-iii.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+
+    void helper(vector<vector<int>>& grid, int r, int c, int zeroes, int cc, int& ans, int n, int m){
+        if(r<0||c<0||r>=n||c>=m) return;
+        if(grid[r][c]==-1) return;
+        if(grid[r][c]==2){
+            if(zeroes+1==cc){
+                ans++;
+            }
+            return;
+        }
+        vector<int> dir={-1, 0, 1, 0, -1};
+        grid[r][c]=-1;
+        for(int i=0; i<4; i++){     
+            helper(grid, r+dir[i], c+dir[i+1], zeroes,cc+1, ans, n, m);    
+        }
+        grid[r][c]=0;
+    }
+
+    int uniquePathsIII(vector<vector<int>>& grid) {
+        int n=grid.size();
+        int m=grid[0].size();
+        int zeroes=0;
+        int r=0, c=0;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(grid[i][j]==1){
+                    r=i, c=j;
+                }
+                if(grid[i][j]==0) zeroes++;
+            }
+        }
+        int ans=0;
+        helper(grid, r, c, zeroes,0, ans, n, m);
+        return ans;
+    }
+};
