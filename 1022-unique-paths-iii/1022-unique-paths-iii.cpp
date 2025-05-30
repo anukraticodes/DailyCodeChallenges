7db64142a20862ada9ae5a7b1cb445cbff26cfce
Anukrati Chaturvedi <146018357+anukraticodes@users.noreply.github.com>
2025-05-30 09:27:39 +0530

    Time: 7 ms (19.76%), Space: 11.4 MB (18.47%) - LeetHub

--- 1022-unique-paths-iii/1022-unique-paths-iii.cpp
+++ 1022-unique-paths-iii/1022-unique-paths-iii.cpp
@@ -11,11 +11,13 @@ public:
             return;
         }
         vector<int> dir={-1, 0, 1, 0, -1};
-        grid[r][c]=-1;
-        for(int i=0; i<4; i++){     
+        
+        for(int i=0; i<4; i++){ 
+            grid[r][c]=-1;    
             helper(grid, r+dir[i], c+dir[i+1], zeroes,cc+1, ans, n, m);    
+            grid[r][c]=0;
         }
-        grid[r][c]=0;
+        
     }
 
     int uniquePathsIII(vector<vector<int>>& grid) {
