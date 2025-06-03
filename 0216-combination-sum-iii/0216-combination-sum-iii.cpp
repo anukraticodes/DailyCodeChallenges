e37bec19c03adb7692cc71f3773c459d0c377eed
Anukrati Chaturvedi <146018357+anukraticodes@users.noreply.github.com>
2025-06-03 15:26:26 +0530

    Time: 0 ms (100%), Space: 8.9 MB (46.08%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.cpp
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -1,34 +1,26 @@
 class Solution {
 public:
 
-set<set<int>> ans;
+vector<vector<int>> res;
     vector<vector<int>> combinationSum3(int k, int n) {
-        set<int> v;
-        helper(k, n, v, 0, 1);
-        vector<vector<int>> res;
-        for(auto it:ans){
-            vector<int> v2;
-            for(auto it2:it){
-                v2.push_back(it2);
-                // cout<<it2<<" ";
-            }
-            res.push_back(v2);
-            // cout<<endl;
-        }
+        vector<int> v;
+        helper(k, n, v, 1);
+        
         return res;
     }
 
-    void helper(int k, int n, set<int> v, int sum, int start){
-        if(v.size()>k || sum>n) return;
+    void helper(int k, int n, vector<int>& v, int start){
+        // if(v.size()>k || start>n) return;
 
-        if(sum==n && v.size()==k){
-            ans.insert(v);
+        if(v.size()==k && n==0){
+            res.push_back(v);
             return;
         }
         for(int i=start; i<=9; i++){
-            v.insert(i);
-            helper(k, n, v, sum+i, i+1);
-            v.erase(i);
+            
+            v.push_back(i);
+            helper(k, n-i, v, i+1);
+            v.pop_back();
         }
     }
 };
