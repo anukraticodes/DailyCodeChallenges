3447bbdc9103861c9382d4f1a808d5a9275be623
Anukrati Chaturvedi <146018357+anukraticodes@users.noreply.github.com>
2025-06-06 09:29:04 +0530

    Time: 20 ms (69.05%), Space: 107.4 MB (42.44%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -2,22 +2,18 @@ class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& temp) {
         vector<int> ans(temp.size());
-        unordered_map<int, int> mp;
-        stack<pair<int, int>> st;
-        st.push({temp[0], 0});
+        stack<int> st;
+        st.push(0);
         for(int i=0; i<temp.size(); i++){
-            while(!st.empty()&&temp[i]>st.top().first){
-                mp[st.top().second]=i-st.top().second;
+            while(!st.empty() && temp[i]>temp[st.top()]){
+                ans[st.top()]=i-st.top();
                 st.pop();
             }
-            st.push({temp[i], i});
+            st.push(i);
         }
         while(!st.empty()){
-            mp[st.top().second]=0;
+            ans[st.top()]=0;
             st.pop();
-        }
-        for(auto it:mp){
-           ans[it.first]=it.second;
         }
         return ans;
     }
