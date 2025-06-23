a13aabaa20ec0d44c229738216488ac102e210f9
Anukrati Chaturvedi <146018357+anukraticodes@users.noreply.github.com>
2025-06-23 12:20:40 +0530

    Time: 31 ms (33.4%), Space: 23.1 MB (34.82%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -2,27 +2,26 @@ class Solution {
 public:
     int numIslands(vector<vector<char>>& grid) {
         int n = grid.size(), m = grid[0].size();
-        vector<vector<bool>> vis(n, vector<bool>(m, 0));
+        // vector<vector<bool>> vis(n, vector<bool>(m, 0));
 
         int cnt = 0;
         vector<int> drc = {-1, 0, 1, 0, -1};
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (grid[i][j] == '1' && !vis[i][j]) {
+                if (grid[i][j] == '1') {
                     cnt++;
                     queue<pair<int, int>> q;
                     q.push({i, j});
-                    vis[i][j] = true;
+                    grid[i][j] = '0';
                     while (!q.empty()) {
                         pair<int, int> p = q.front();
                         q.pop();
-                        vis[p.first][p.second] = 1;
+                        grid[p.first][p.second] = '0';
                         for (int k = 0; k < 4; k++) {
                             int nr = p.first + drc[k],
                                 nc = p.second + drc[k + 1];
-                            if (nr >= 0 && nc >= 0 && nr < n && nc < m &&
-                                !vis[nr][nc] && grid[nr][nc] == '1') {
-                                vis[nr][nc] = 1;
+                            if (nr >= 0 && nc >= 0 && nr < n && nc < m && grid[nr][nc] == '1') {
+                                grid[nr][nc] = '0';
                                 q.push({nr, nc});
                             }
                         }
