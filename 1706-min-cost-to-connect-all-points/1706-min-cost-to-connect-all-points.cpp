2932cc25104fad36cc5945b35b74553ffc9c28f8
Anukrati Chaturvedi <146018357+anukraticodes@users.noreply.github.com>
2025-06-26 15:17:18 +0530

    Time: 498 ms (27.78%), Space: 63.9 MB (45.65%) - LeetHub

--- 1706-min-cost-to-connect-all-points/1706-min-cost-to-connect-all-points.cpp
+++ 1706-min-cost-to-connect-all-points/1706-min-cost-to-connect-all-points.cpp
@@ -1,33 +1,52 @@
 class Solution {
 public:
-    int minCostConnectPoints(vector<vector<int>>& points) {
-        int n=points.size();
-        vector<vector<pair<int, int>>> adj(n);
-        int k=0;
+    vector<int> parent, rank;
+    void createDsu(int n){
+        parent.resize(n);
+        rank.resize(n, 0);
         for(int i=0; i<n; i++){
-            int x=points[i][0], y=points[i][1];
-        for(int j=0; j<n; j++){
-            if(points[i]==points[j]) continue;
-            int x1=points[j][0], y1=points[j][1];
-            int dist=abs(x-x1)+abs(y-y1);
-            adj[i].push_back({j, dist});
+            parent[i]=i;
         }
+    }
+
+    int findParent(int node){
+        if(parent[node]==node) return node;
+        return parent[node]=findParent(parent[node]);
+    }
+
+    void unionDsu(int u, int v){
+        int pu=findParent(u), pv=findParent(v);
+        if(pu==pv) return;
+        if(rank[pu] > rank[pv]){
+        parent[pv] = pu;
+    } else if(rank[pu] < rank[pv]){
+        parent[pu] = pv;
+    } else {
+        parent[pu] = pv;
+        rank[pv]++;
+    }
+    }
+
+
+    int minCostConnectPoints(vector<vector<int>>& points) {
+        if(points.size()==0) return 0;
+        priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>, greater<tuple<int, int, int>>> pq;
+        for(int i=0; i<points.size()-1; i++){
+            int x=points[i][0], y=points[i][1];
+            for(int j=i+1; j<points.size(); j++){
+                int x1=points[j][0], y1=points[j][1];
+                int dist=abs(x-x1)+abs(y-y1);
+                pq.push({dist, i, j});
+            }
         }
         int sum=0;
-        vector<bool> vis(n, 0);
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-        pq.push({0,0});
+        createDsu(points.size());
         while(!pq.empty()){
-            int wt=pq.top().first;
-            int node=pq.top().second;
+            auto[wt, u, v]=pq.top();
             pq.pop();
-            if(vis[node]) continue;
-            vis[node]=1;
-            sum+=wt;
-            for(auto nbr:adj[node]){
-                if(!vis[nbr.first]){
-                    pq.push({nbr.second, nbr.first});
-                }
+            if(findParent(u)!=findParent(v)){
+                unionDsu(u, v);
+                sum+=wt;
             }
         }
         return sum;
