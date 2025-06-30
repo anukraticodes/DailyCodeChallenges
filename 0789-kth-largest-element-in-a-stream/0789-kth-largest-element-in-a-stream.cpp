253639b389c6e3dc31a0cf8afb016108466cf036
Anukrati Chaturvedi <146018357+anukraticodes@users.noreply.github.com>
2025-06-30 16:25:20 +0530

    Time: 13 ms (15.69%), Space: 33.2 MB (23.52%) - LeetHub

--- 0789-kth-largest-element-in-a-stream/0789-kth-largest-element-in-a-stream.cpp
+++ 0789-kth-largest-element-in-a-stream/0789-kth-largest-element-in-a-stream.cpp
@@ -1,18 +1,18 @@
 class KthLargest {
-    int num;
-    priority_queue<int, vector<int>, greater<int>> pq;
 public:
+priority_queue<int, vector<int>, greater<int>> pq;
+int n;
     KthLargest(int k, vector<int>& nums) {
-        num=k;
-        for(auto ele: nums){
-            pq.push(ele);
-            if(pq.size()>k)  pq.pop();
+        n=k;
+        for(int i=0; i<nums.size(); i++){
+            pq.push(nums[i]);
+            if(pq.size()>k) pq.pop();
         }
     }
     
     int add(int val) {
         pq.push(val);
-        if(pq.size()>num) pq.pop();
+        if(pq.size()>n) pq.pop();
         return pq.top();
     }
 };
