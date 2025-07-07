c890b51f490b4574f3673e91618c343463a7df33
Anukrati Chaturvedi <146018357+anukraticodes@users.noreply.github.com>
2025-07-07 13:28:55 +0530

    Time: 60 ms (69.12%), Space: 75 MB (39.3%) - LeetHub

--- 1478-maximum-number-of-events-that-can-be-attended/1478-maximum-number-of-events-that-can-be-attended.cpp
+++ 1478-maximum-number-of-events-that-can-be-attended/1478-maximum-number-of-events-that-can-be-attended.cpp
@@ -1,32 +1,25 @@
 class Solution {
 public:
-    vector<int> parent;
-
-    int find(int day) {
-        if (parent[day] != day)
-            parent[day] = find(parent[day]); 
-        return parent[day];
-    }
-
-    void unite(int u, int v) { parent[u] = v; }
-
     int maxEvents(vector<vector<int>>& events) {
         int cnt = 0;
-        sort(events.begin(), events.end(),
-             [](const auto& a, const auto& b) { return a[1] < b[1]; });
-        int n = events[events.size() - 1][1];
-        parent.resize(n + 2);
-        for (int i = 0; i <= n + 1; i++)
-            parent[i] = i;
-        for (auto it : events) {
-            int u = it[0], v = it[1];
-            int avail = find(u);
-            if (avail <= v) {
-                unite(avail, avail + 1);
+        int n=events.size();
+        sort(events.begin(), events.end());
+        int maxDay = 0;
+        for (int i = 0; i < events.size(); i++) {
+            maxDay = max(maxDay, events[i][1]);
+        }
+        priority_queue<int, vector<int>, greater<int>> pq;
+        for(int i=0, j=0; i<=maxDay; i++){
+            while(j<n && events[j][0]<=i){
+                pq.push(events[j][1]);
+                j++;
+            }
+            while(!pq.empty() && pq.top()<i) pq.pop();
+            if(!pq.empty()){
+                pq.pop();
                 cnt++;
             }
         }
-
         return cnt;
     }
 };
