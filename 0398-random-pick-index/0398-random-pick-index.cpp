d508bffb33cbb50460b5afbeff753ca5b7787bb2
Anukrati Chaturvedi <146018357+anukraticodes@users.noreply.github.com>
2025-07-25 18:32:27 +0530

    Time: 36 ms (87.92%), Space: 66.7 MB (70.04%) - LeetHub

--- 0398-random-pick-index/0398-random-pick-index.cpp
+++ 0398-random-pick-index/0398-random-pick-index.cpp
@@ -1,24 +1,17 @@
 class Solution {
     private:
-    vector<int> nums;
+    unordered_map<int, vector<int>> ind;
 public:
 
     Solution(vector<int>& nums) {
-        this->nums.swap(nums);
+        for(int i=0; i<nums.size(); i++){
+            this->ind[nums[i]].push_back(i);
+        }
     }
     
     int pick(int target) {
-        vector<int> arr;
-        for(int i=0; i<nums.size(); i++){
-            if(nums[i]==target) arr.push_back(i);
-        }
-        int size = arr.size();
-        
-
-    int randomIndex = rand() % size;
-
-    int randomValue = arr[randomIndex];
-    return randomValue;
+        int size=ind[target].size();
+        return ind[target][rand()%size];
     }
 };
 
