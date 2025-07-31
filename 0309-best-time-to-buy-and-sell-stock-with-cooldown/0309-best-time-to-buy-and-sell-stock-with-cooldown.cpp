1b8ab6090e1168c75a4d274fe0738c8ff508c7df
Anukrati Chaturvedi <146018357+anukraticodes@users.noreply.github.com>
2025-07-31 12:11:19 +0530

    Time: 3 ms (35.35%), Space: 16.3 MB (22.69%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
 
-    int helper(vector<vector<vector<int>>>& dp, vector<int>& p, int ind, bool buy, bool cooldown){
-        if(ind==p.size()) return 0;
-        if(dp[ind][buy][cooldown]!=-1) return dp[ind][buy][cooldown];
+    int helper(vector<vector<int>>& dp, vector<int>& p, int ind, bool buy){
+        if(ind>=p.size()) return 0;
+        if(dp[ind][buy]!=-1) return dp[ind][buy];
         int profit=0;
         if(buy==0){
-            if (cooldown)
-                profit = helper(dp, p, ind + 1, 0, 0);
-            else
-            profit=max(helper(dp, p, ind+1, 0, 0), -p[ind]+helper(dp, p, ind+1, 1, 0));
+            profit=max(helper(dp, p, ind+1, 0), -p[ind]+helper(dp, p, ind+1, 1));
         }
         else{
-            profit=max(helper(dp, p, ind+1, 1, 0), p[ind]+helper(dp, p, ind+1, 0, 1));
+            profit=max(helper(dp, p, ind+1, 1), p[ind]+helper(dp, p, ind+2, 0));
         }
-        return dp[ind][buy][cooldown]=profit;
+        return dp[ind][buy]=profit;
     }
 
     int maxProfit(vector<int>& p) {
         int n=p.size();
-        vector<vector<vector<int>>> dp(n, vector<vector<int>>(2, vector<int>(2, -1)));
-        return helper(dp, p, 0, 0, 0);
+        vector<vector<int>> dp(n, vector<int>(2, -1));
+        return helper(dp, p, 0, 0);
     }
 };
