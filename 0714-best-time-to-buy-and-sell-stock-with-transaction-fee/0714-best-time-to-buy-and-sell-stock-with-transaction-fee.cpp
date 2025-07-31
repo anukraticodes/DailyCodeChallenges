c1e9d78c420e3d32dc6fd67d87276cb1e3343d4e
Anukrati Chaturvedi <146018357+anukraticodes@users.noreply.github.com>
2025-07-31 12:19:26 +0530

    Time: 117 ms (20.59%), Space: 100.5 MB (12.41%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -1,12 +1,22 @@
 class Solution {
 public:
-    int maxProfit(vector<int>& prices, int fee) {
-        int buy=INT_MIN;
-        int sell=0;
-        for(int price:prices){
-            buy=max(buy, sell-price);
-            sell=max(sell, buy+price-fee);
+
+    int helper(vector<vector<int>>& dp, vector<int>& p, int ind, bool buy, int fee){
+        if(ind==p.size()) return 0;
+        if(dp[ind][buy]!=-1) return dp[ind][buy];
+        int profit=0;
+        if(buy==0){
+            profit=max(helper(dp, p, ind+1, 0, fee), -p[ind]+helper(dp, p, ind+1, 1, fee));
         }
-        return sell;
+        else{
+            profit=max(helper(dp, p, ind+1, 1, fee), p[ind]-fee+helper(dp, p, ind+1, 0, fee));
+        }
+        return dp[ind][buy]=profit;
+    }
+
+    int maxProfit(vector<int>& p, int fee) {
+        int n=p.size();
+        vector<vector<int>> dp(n, vector<int>(2, -1));
+        return helper(dp, p, 0, 0, fee);
     }
 };
