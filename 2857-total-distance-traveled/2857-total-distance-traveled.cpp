76ca7e8f6d1dbab06a7ec5b4f06afe55d8b50f95
Anukrati Chaturvedi <146018357+anukraticodes@users.noreply.github.com>
2025-08-09 21:21:02 +0530

    Time: 0 ms (100%), Space: 11.2 MB (76.61%) - LeetHub

--- 2857-total-distance-traveled/2857-total-distance-traveled.cpp
+++ 2857-total-distance-traveled/2857-total-distance-traveled.cpp
@@ -1,19 +1,6 @@
 class Solution {
 public:
-    int distanceTraveled(int mainTank, int add) {
-        int cnt=0;
-        while(mainTank){
-          if(mainTank<5) return cnt+(mainTank*10);
-          if(add!=0){
-            mainTank-=4;
-            add--;
-            cnt+=50;
-          }
-          else{
-            mainTank-=5;
-            cnt+=50;
-          }
-        }
-        return cnt;
+    int distanceTraveled(int a, int b) {
+        return (a+min((a-1)/4, b))*10;
     }
 };
