08e5fa88ea35c47f1b693187a276d16d1c9e49b7
Anukrati Chaturvedi <146018357+anukraticodes@users.noreply.github.com>
2025-08-11 09:38:17 +0530

    Time: 18 ms (50.8%), Space: 53.1 MB (59%) - LeetHub

--- 2728-sum-in-a-matrix/2728-sum-in-a-matrix.cpp
+++ 2728-sum-in-a-matrix/2728-sum-in-a-matrix.cpp
@@ -5,13 +5,11 @@ public:
         for(int i=0; i<nums.size(); i++){
             sort(nums[i].begin(), nums[i].end(), greater<int>());
         }
-        int k=0;
         for(int i=0; i<nums[0].size(); i++){
             int val=INT_MIN;
             for(int j=0; j<nums.size(); j++){
-               val=max(val, nums[j][k]);
+               val=max(val, nums[j][i]);
             }
-            k++;
             cnt+=val;
         }
         return cnt;
