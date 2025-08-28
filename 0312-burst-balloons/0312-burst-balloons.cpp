00ce633edf02270768a50598ea2c5252dfd38e2e
Anukrati Chaturvedi <146018357+anukraticodes@users.noreply.github.com>
2025-08-28 10:43:59 +0530

    Time: 205 ms (26.93%), Space: 13.9 MB (28.92%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -1,34 +1,27 @@
 class Solution {
 public:
 
-    int helper(vector<int>& nums, int i, int j, vector<vector<int>>& dp){
+    int helper(vector<vector<int>>& dp, vector<int>& nums, int i, int j){
         int n=nums.size();
         if(i>j) return 0;
-        if(i==j){
-            int temp=nums[i];
-            if(i-1>=0) temp*=nums[i-1];
-            if(i+1<n) temp*=nums[i+1];
-            return temp;
-        }
         if(dp[i][j]!=-1) return dp[i][j];
-        int ans=0;
-        for(int k=i; k<=j; k++){
-            int temp=nums[k];
-            if(j+1<n) temp*=nums[j+1];
-            if(i-1>=0) temp*=nums[i-1];
-            temp+=(helper(nums, i, k-1, dp)+helper(nums, k+1, j, dp));
-            ans=max(temp, ans);
-        }
-        return dp[i][j]=ans;
+        int ans = 0;
+    for (int k = i; k <= j; k++) {
+        int coins = nums[i-1] * nums[k] * nums[j+1] 
+                    + helper(dp, nums, i, k-1) 
+                    + helper(dp, nums, k+1, j);
+        ans = max(ans, coins);
+    }
+    return dp[i][j] = ans;
     }
 
     int maxCoins(vector<int>& nums) {
-      
-        vector<int> arr={1};
-        for(auto n:nums) arr.push_back(n);
-        arr.push_back(1);
-        vector<vector<int>> dp(501, vector<int>(501, -1));
-        int n=arr.size();
-        return helper(arr, 1, n-2, dp);
+        vector<int> nnums;
+        nnums.push_back(1);
+        for(auto n:nums) nnums.push_back(n);
+        nnums.push_back(1);
+        int m=nnums.size();
+        vector<vector<int>> dp(m, vector<int>(m, -1));
+        return helper(dp, nnums, 1, m-2);
     }
 };
