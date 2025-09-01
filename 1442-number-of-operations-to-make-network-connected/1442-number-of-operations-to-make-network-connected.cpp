15969587c7b58c4b6e862d1fb0e72ebcdf16e825
Anukrati Chaturvedi <146018357+anukraticodes@users.noreply.github.com>
2025-09-01 20:35:02 +0530

    Time: 7 ms (75.47%), Space: 42 MB (97.15%) - LeetHub

--- 1442-number-of-operations-to-make-network-connected/1442-number-of-operations-to-make-network-connected.cpp
+++ 1442-number-of-operations-to-make-network-connected/1442-number-of-operations-to-make-network-connected.cpp
@@ -1,49 +1,45 @@
 class Solution {
 public:
-vector<int> parent, rank;
+    vector<int> parent, rank;
 
-void disjoint(int n){
-    rank.resize(n);
-    parent.resize(n);
-    for(int i=0; i<n; i++){
+    void dsu(int n){
+      parent.resize(n);
+      rank.resize(n, 0);
+      for(int i=0; i<n; i++){
         parent[i]=i;
-        rank[i]=0;
+      }
     }
-}
 
-void join(int u, int v){
-    int pu=findP(u);
-    int pv=findP(v);
-    if(rank[pu]>rank[pv]){
-        parent[pv]=pu;
+    int findP(int u){
+        if(parent[u]==u) return parent[u];
+        else return parent[u]=findP(parent[u]);
     }
-    else if(rank[pv]>rank[pu]){
-        parent[pu]=pv;
+
+    void merge(int u, int v){
+        int pu=findP(u);
+        int pv=findP(v);
+        if(pu==pv) return;
+        if(rank[pu]>rank[pv]){
+            parent[pv]=pu;
+        }
+        else if(rank[pu]<rank[pv]) parent[pu]=pv;
+        else{
+            parent[pu]=pv;
+            rank[pv]++;
+        }
     }
-    else{
-        parent[pu]=pv;
-        rank[pv]++;
-    }
-}
-
-int findP(int u){
-    if(parent[u]==u) return u;
-    return parent[u]=findP(parent[u]);
-}
-
-
 
     int makeConnected(int n, vector<vector<int>>& c) {
-        disjoint(n);
-        if((c.size())<n-1) return -1;
+        int n1=c.size();
+        if(n1<n-1) return -1;
+        dsu(n);
         int cnt=0;
         for(int i=0; i<c.size(); i++){
-            join(c[i][0], c[i][1]);
+            merge(c[i][0], c[i][1]);
         }
         for(int i=0; i<n; i++){
-            if(parent[i]==i)cnt++;
+            if(parent[i]==i) cnt++;
         }
         return cnt-1;
-
     }
 };
