df933c001878fda08933208e22b0ceb7f0777ad5
Anukrati Chaturvedi <146018357+anukraticodes@users.noreply.github.com>
2025-10-08 15:13:03 +0530

    Time: 0 ms (100%), Space: 17.7 MB (17.71%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,25 +1,47 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        int fst=-1, snd=-1;
-        fst=bs(nums, target, 1);
-        snd=bs(nums, target, 0);
-        return {fst, snd};
+        int n = (int)nums.size();
+        if (n == 0) return {-1, -1};
+
+        int st  = helper1(nums, target); // last index < target
+        int end = helper2(nums, target); // first index > target
+
+        int L = st + 1;
+        int R = end - 1;
+
+        if (L <= R && L >= 0 && R < n && nums[L] == target && nums[R] == target)
+            return {L, R};
+        return {-1, -1};
     }
 
-    int bs(vector<int>& nums, int target, bool l){
-        int low=0, high=nums.size()-1, ind=-1;
-
-        while(low<=high){
-            int mean=low+(high-low)/2;
-            if(nums[mean]>target) high=mean-1;
-            else if(nums[mean]<target) low=mean+1;
-            else{
-                ind=mean;
-                if(l) high=mean-1;
-                else low=mean+1;
+    // last index with value < target
+    int helper1(vector<int>& nums, int target) {
+        int l = 0, r = (int)nums.size() - 1, ans = -1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (nums[mid] < target) {
+                ans = mid;          // valid (< target)
+                l = mid + 1;        // push right to find last <
+            } else {
+                r = mid - 1;        // nums[mid] >= target
             }
         }
-        return ind;
+        return ans; // -1 if none <
+    }
+
+    // first index with value > target
+    int helper2(vector<int>& nums, int target) {
+        int l = 0, r = (int)nums.size() - 1, ans = (int)nums.size();
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (nums[mid] > target) {
+                ans = mid;          // valid (> target)
+                r = mid - 1;        // pull left to find first >
+            } else {
+                l = mid + 1;        // nums[mid] <= target
+            }
+        }
+        return ans; // n if none >
     }
 };
