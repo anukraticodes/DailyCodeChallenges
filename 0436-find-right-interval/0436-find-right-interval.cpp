2f7784f5733e546efe0a2db861e79d9482732b29
Anukrati Chaturvedi <146018357+anukraticodes@users.noreply.github.com>
2025-10-09 17:48:05 +0530

    Time: 19 ms (44.86%), Space: 33.1 MB (20.62%) - LeetHub

--- 0436-find-right-interval/0436-find-right-interval.cpp
+++ 0436-find-right-interval/0436-find-right-interval.cpp
@@ -1,35 +1,41 @@
 class Solution {
 public:
     vector<int> findRightInterval(vector<vector<int>>& intervals) {
-       vector<int> temp;
-       unordered_map<int, int> mp;
-       for(int i=0; i<intervals.size(); i++) {temp.push_back(intervals[i][0]); mp[intervals[i][0]]=i;}
-        if(intervals.size()<1) return {-1};
+        vector<int> temp;
+        unordered_map<int, int> mp;
+        for (int i = 0; i < intervals.size(); i++) {
+            temp.push_back(intervals[i][0]);
+            mp[intervals[i][0]] = i;
+        }
+        if (intervals.size() < 1)
+            return {-1};
         sort(temp.begin(), temp.end());
         vector<int> ans;
-        int outlier=temp[0]-1;
-        for(int i=0; i<intervals.size(); i++){
-            
-            int t=helper(temp, intervals[i][1], outlier);
-            if(t==outlier){ans.push_back(-1); continue;}
+        int outlier = temp[0] - 1;
+
+        for (int i = 0; i < intervals.size(); i++) {
+            int t = helper(temp, intervals[i][1], outlier);
+            if (t == outlier) {
+                ans.push_back(-1);
+                continue;
+            }
             ans.push_back(mp[t]);
         }
+
         return ans;
     }
 
-    int helper(vector<int>& intervals, int target, int outlier){
-        int l=0, r=intervals.size()-1;
-        while(l<=r){
-            int mid=(l+r)/2;
-            if(intervals[mid]<target) l=mid+1;
-            else if(intervals[mid]>=target) r=mid-1;
+    int helper(vector<int>& intervals, int target, int outlier) {
+        int l = 0, r = intervals.size() - 1;
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            if (intervals[mid] < target)
+                l = mid + 1;
+            else if (intervals[mid] >= target)
+                r = mid - 1;
         }
-        if(l==intervals.size()) return outlier;
+        if (l == intervals.size())
+            return outlier;
         return intervals[l];
-    }
-
-    int ind(int t, vector<vector<int>>& inte){
-        for(int i=0; i<inte.size(); i++) if(inte[i][0]==t) return i;
-        return -1;
     }
 };
