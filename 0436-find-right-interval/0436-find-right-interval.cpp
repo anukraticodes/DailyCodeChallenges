2bdec7fc071f088d03445fcb1adb385d20e1161c
Anukrati Chaturvedi <146018357+anukraticodes@users.noreply.github.com>
2025-10-09 17:45:22 +0530

    Time: 403 ms (11.35%), Space: 29.4 MB (77.47%) - LeetHub

--- 0436-find-right-interval/0436-find-right-interval.cpp
+++ 0436-find-right-interval/0436-find-right-interval.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     vector<int> findRightInterval(vector<vector<int>>& intervals) {
-       vector<vector<int>> temp=intervals;
+       vector<int> temp;
+       for(int i=0; i<intervals.size(); i++) temp.push_back(intervals[i][0]);
         if(intervals.size()<1) return {-1};
         sort(temp.begin(), temp.end());
         vector<int> ans;
-        int outlier=temp[0][0]-1;
+        int outlier=temp[0]-1;
         for(int i=0; i<intervals.size(); i++){
             
             int t=helper(temp, intervals[i][1], outlier);
@@ -15,15 +16,15 @@ public:
         return ans;
     }
 
-    int helper(vector<vector<int>>& intervals, int target, int outlier){
+    int helper(vector<int>& intervals, int target, int outlier){
         int l=0, r=intervals.size()-1;
         while(l<=r){
             int mid=(l+r)/2;
-            if(intervals[mid][0]<target) l=mid+1;
-            else if(intervals[mid][0]>=target) r=mid-1;
+            if(intervals[mid]<target) l=mid+1;
+            else if(intervals[mid]>=target) r=mid-1;
         }
         if(l==intervals.size()) return outlier;
-        return intervals[l][0];
+        return intervals[l];
     }
 
     int ind(int t, vector<vector<int>>& inte){
