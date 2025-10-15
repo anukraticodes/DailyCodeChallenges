55d025bda567c9bdc2de10ad1432a877a63c3747
Anukrati Chaturvedi <146018357+anukraticodes@users.noreply.github.com>
2025-10-15 20:24:17 +0530

    Time: 0 ms (100%), Space: 8.5 MB (85.09%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -3,6 +3,7 @@ public:
     int mySqrt(int x) {
         int l=1, r=x;
         while(l<=r){
+            
             long long mid=l+(r-l)/2;
             if((mid*mid)<=(long long)x) l=mid+1;
             else r=mid-1;
