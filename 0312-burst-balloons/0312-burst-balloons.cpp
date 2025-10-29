3052c0e1313b38e7ed78609367e7071853ffbab2
Anukrati Chaturvedi <146018357+anukraticodes@users.noreply.github.com>
2025-10-29 16:35:00 +0530

    Time: 103 ms (66.32%), Space: 14 MB (29.1%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -1,20 +1,6 @@
 class Solution {
 public:
 
-    // int helper(vector<vector<int>>& dp, vector<int>& nums, int i, int j){
-    //     int n=nums.size();
-    //     if(i>j) return 0;
-    //     if(dp[i][j]!=-1) return dp[i][j];
-    //     int ans = 0;
-    // for (int k = i; k <= j; k++) {
-    //     int coins = nums[i-1] * nums[k] * nums[j+1] 
-    //                 + helper(dp, nums, i, k-1) 
-    //                 + helper(dp, nums, k+1, j);
-    //     ans = max(ans, coins);
-    // }
-    // return dp[i][j] = ans;
-    // }
-
     int maxCoins(vector<int>& nums) {
         vector<int> nnums;
         nnums.push_back(1);
