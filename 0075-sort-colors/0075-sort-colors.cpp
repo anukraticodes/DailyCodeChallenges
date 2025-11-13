0452dcf51e4893dbbb26531ec83e595e598fc7c0
Anukrati Chaturvedi <146018357+anukraticodes@users.noreply.github.com>
2025-11-13 17:45:02 +0530

    Time: 0 ms (100%), Space: 15.4 MB (11.99%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,6 +1,30 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
+        mergesort(nums, 0, nums.size()-1);
+    }
+
+    void mergesort(vector<int>& nums, int l, int r){
+        if(l<r){
+            int mid=(l+r)/2;
+            mergesort(nums, l, mid);
+            mergesort(nums, mid+1, r);
+            combine(nums, l, r, mid);
+        }
+    }
+
+    void combine(vector<int>& nums, int l, int r, int mid){
+        int n1=mid-l+1;
+        int n2=r-mid;
+        vector<int> L(n1), R(n2);
+        for(int i=0; i<n1; i++) L[i]=nums[l+i];
+        for(int j=0; j<n2; j++) R[j]=nums[mid+j+1];
+        int i=0, j=0, k=l;
+        while(i<n1 && j<n2){
+            if(L[i]<=R[j]) nums[k++]=L[i++];
+            else nums[k++]=R[j++];
+        }
+        while(i<n1) nums[k++]=L[i++];
+        while(j<n2) nums[k++]=R[j++];
     }
 };
