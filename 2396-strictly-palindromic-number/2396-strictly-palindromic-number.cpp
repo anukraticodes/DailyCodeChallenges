f4aa30260a416cc171899e6f642c251505df57c5
Anukrati Chaturvedi <146018357+anukraticodes@users.noreply.github.com>
2025-12-17 20:09:42 +0530

    Time: 0 ms (100%), Space: 8.1 MB (5.66%) - LeetHub

--- 2396-strictly-palindromic-number/2396-strictly-palindromic-number.cpp
+++ 2396-strictly-palindromic-number/2396-strictly-palindromic-number.cpp
@@ -1,6 +1,18 @@
 class Solution {
 public:
     bool isStrictlyPalindromic(int n) {
-        return 0;
+        for(int i=2; i<=n-2; i++){
+            int temp=n;
+            string d="";
+            while(temp>0){
+                int rem=temp%i;
+                d=to_string(rem)+d;
+                temp/=i;
+            }
+            string rev=d;
+            reverse(rev.begin(), rev.end());
+            if(rev!=d) return 0;
+        }
+        return 1;
     }
 };
