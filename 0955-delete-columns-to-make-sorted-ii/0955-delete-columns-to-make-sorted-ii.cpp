c0f5551977cde7732cfe88cc5fd45675626be5ac
Anukrati Chaturvedi <146018357+anukraticodes@users.noreply.github.com>
2025-12-21 15:03:03 +0530

    Time: 0 ms (100%), Space: 12.9 MB (62.92%) - LeetHub

--- 0955-delete-columns-to-make-sorted-ii/0955-delete-columns-to-make-sorted-ii.cpp
+++ 0955-delete-columns-to-make-sorted-ii/0955-delete-columns-to-make-sorted-ii.cpp
@@ -1,40 +1,35 @@
 class Solution {
 public:
     int minDeletionSize(vector<string>& strs) {
+        int n = strs.size();
+        int m = strs[0].size();
+
+        vector<bool> resolved(n - 1, false);
+        int unresolved = n - 1;
         int deletions = 0;
-        vector<bool> resolved(strs.size() - 1, false);
-        isordered(0, resolved, strs, deletions);
+
+        for (int col = 0; col < m && unresolved > 0; col++) {
+            bool bad = false;
+            for (int i = 0; i < n - 1; i++) {
+                if (!resolved[i] && strs[i][col] > strs[i + 1][col]) {
+                    bad = true;
+                    break;
+                }
+            }
+
+            if (bad) {
+                deletions++;
+                continue;
+            }
+
+            for (int i = 0; i < n - 1; i++) {
+                if (!resolved[i] && strs[i][col] < strs[i + 1][col]) {
+                    resolved[i] = true;
+                    unresolved--;
+                }
+            }
+        }
+
         return deletions;
     }
-
-    bool isordered(int col,
-               vector<bool>& resolved,
-               vector<string>& strs,
-               int& deletions)
-{
-    int n = strs.size();
-    if (col == strs[0].size())
-        return true;
-
-    // check if this column must be deleted
-    for (int i = 0; i < n - 1; i++) {
-        if (!resolved[i] && strs[i][col] > strs[i+1][col]) {
-            deletions++;
-            return isordered(col + 1, resolved, strs, deletions);
-        }
-    }
-
-    // mark newly resolved pairs
-    vector<bool> newResolved = resolved;
-    for (int i = 0; i < n - 1; i++) {
-        if (!resolved[i] && strs[i][col] < strs[i+1][col]) {
-            newResolved[i] = true;
-        }
-    }
-
-    return isordered(col + 1, newResolved, strs, deletions);
-}
-
-
-
 };
