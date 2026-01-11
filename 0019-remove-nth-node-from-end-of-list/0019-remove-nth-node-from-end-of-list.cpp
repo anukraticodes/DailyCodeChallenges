e6659c4326a416cd673637c6dee423f774f2fdd9
Anukrati Chaturvedi <146018357+anukraticodes@users.noreply.github.com>
2026-01-11 17:18:14 +0530

    Time: 0 ms (100%), Space: 14.9 MB (58.78%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -1,39 +1,19 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        int size=0;
-        ListNode* node=head;
-        while(node!=nullptr){
-          node=node->next;
-          size++;
-        }
-        if (n == size) {
-        ListNode* temp = head;
-        head = head->next;
-        delete temp;
-        return head;
-    }
-        int a=size-n-1;
-        size=0;
-        node=head;
-        while(size<a){
-          node=node->next;
-          size+=1;
-        }
-        ListNode* temp = node->next;
-    node->next = node->next->next;
-    delete temp;
+        ListNode* res = new ListNode(0, head);
+        ListNode* dummy = res;
 
-    return head;
+        for (int i = 0; i < n; i++) {
+            head = head->next;
+        }
+
+        while (head != nullptr) {
+            head = head->next;
+            dummy = dummy->next;
+        }
+
+        dummy->next = dummy->next->next;
+        return res->next;        
     }
 };
