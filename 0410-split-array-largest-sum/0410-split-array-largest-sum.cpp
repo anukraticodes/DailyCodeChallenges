196039c136e01e90aa77b5601f314b12610dbee2
Anukrati Chaturvedi <146018357+anukraticodes@users.noreply.github.com>
2026-01-23 18:22:30 +0530

    Time: 0 ms (100%), Space: 10.5 MB (57.06%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
-    bool helper(vector<int>& nums, int k, int mid){
-        int cnt=1, total=0;
-        for(auto num:nums){
-            total+=num;
-            if(total>mid){
-                total=num;
-                cnt++;
-                if(cnt>k) return 0;
+
+    bool possible(vector<int>& nums, int k, int mid){
+        int cnt=1, sum=0;
+        for(int r=0; r<nums.size(); r++){
+            sum+=nums[r];
+            if(sum>mid){
+                sum=nums[r];
+                if(++cnt >k) return 0;
             }
-            
         }
         return 1;
     }
@@ -18,8 +17,8 @@ public:
         int l=*max_element(nums.begin(), nums.end());
         int r=accumulate(nums.begin(), nums.end(), 0);
         while(l<r){
-            int mid=l+(r-l)/2;
-            if(helper(nums, k, mid)) r=mid;
+            int mid=(l+r)/2;
+            if(possible(nums, k, mid)) r=mid;
             else l=mid+1;
         }
         return l;
