4e26fdd8ae12ccd40532acd77926e9e620906f1b
Anukrati Chaturvedi <146018357+anukraticodes@users.noreply.github.com>
2026-01-23 18:37:45 +0530

    Time: 4 ms (87.57%), Space: 23 MB (11.93%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -15,9 +15,9 @@ public:
         int ans=0;
         while(l<=r){
             int mid=l+(r-l)/2;
-            if(helper(piles, mid, h)){ans=mid; r=mid-1;}
+            if(helper(piles, mid, h)){r=mid-1;}
             else l=mid+1;
         }
-        return ans;
+        return r+1;
     }
 };
