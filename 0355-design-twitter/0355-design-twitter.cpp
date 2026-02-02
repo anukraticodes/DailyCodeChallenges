11de945ff0396fb70e0e9e39999a4e5eac40d7b7
Anukrati Chaturvedi <146018357+anukraticodes@users.noreply.github.com>
2026-02-02 17:33:43 +0530

    Time: 4 ms (83.18%), Space: 58.5 MB (11.91%) - LeetHub

--- 0355-design-twitter/0355-design-twitter.cpp
+++ 0355-design-twitter/0355-design-twitter.cpp
@@ -2,25 +2,37 @@ class Twitter {
 public:
 
 vector<unordered_set<int>> v;
-vector<pair<int, int>> t;
+unordered_map<int, vector<pair<int,int>>> tweets;
+int timer=0;
+
     Twitter() {
         v.resize(501);
         for(int i=1; i<=500; i++){
             v[i].insert(i);
         }
+        
     }
     
     void postTweet(int userId, int tweetId) {
-       t.push_back({userId, tweetId});
+       tweets[userId].push_back({timer++, tweetId});
     }
     
     vector<int> getNewsFeed(int userId) {
         vector<int> ans;
-        int cnt=0;
-        for(int i=t.size()-1; i>=0; i--){
-            if(v[userId].count(t[i].first)){
-                ans.push_back(t[i].second);
-                if(++cnt == 10) break;
+        priority_queue<tuple<int, int, int, int>> pq;
+        for(auto it:v[userId]){
+            if(!tweets[it].empty()){
+                int idx=tweets[it].size()-1;
+                pq.push({tweets[it][idx].first, tweets[it][idx].second, it, idx});
+            }
+        }
+
+        while(!pq.empty() && ans.size()<10){
+            auto [time, id, ind, idx]=pq.top();
+            pq.pop();
+            ans.push_back(id);
+            if(idx>0){
+                pq.push({tweets[ind][idx-1].first, tweets[ind][idx-1].second, ind, idx-1});
             }
         }
         return ans;
