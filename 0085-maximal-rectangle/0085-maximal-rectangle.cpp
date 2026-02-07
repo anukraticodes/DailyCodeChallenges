ef8edaaa67476c9db3546fb56638d74170e6a02e
Anukrati Chaturvedi <146018357+anukraticodes@users.noreply.github.com>
2026-02-07 11:09:55 +0530

    Time: 0 ms (100%), Space: 17.2 MB (87.94%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.cpp
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
-    int maximalRectangle(vector<vector<char>>& mat) {
-        int n=mat.size(), m=mat[0].size();
+    int maximalRectangle(vector<vector<char>>& matrix) {
+        int n=matrix.size(), m=matrix[0].size();
+        vector<int> h(201,0), st(201, -1);
         int ans=0;
-        vector<int> h(201,0), st(201,-1);
         for(int i=0; i<n; i++){
             int top=0;
             for(int j=0; j<=m; j++){
-               h[j]=(j==m || mat[i][j]=='0')?0:h[j]+1;
-
-               while(top>0 && (j==m || h[j]<h[st[top]])){
-                int m=st[top--];
-                int w=j-st[top]-1;
-                int a=h[m]*w;
-                ans=max(ans, a);
-               }
-               st[++top]=j;
+                h[j]=(j==m || matrix[i][j]=='0')?0:h[j]+1;
+                while(top>0 && (j==m || h[st[top]]>h[j])){
+                    int height=st[top--];
+                    int w=j-st[top]-1;
+                    ans=max(ans, h[height]*w);
+                }
+                st[++top]=j;
             }
         }
         return ans;
