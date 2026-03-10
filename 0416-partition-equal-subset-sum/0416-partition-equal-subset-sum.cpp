585f2381798f3677537af64b4b71fef083e0d57f
Anukrati Chaturvedi <146018357+anukraticodes@users.noreply.github.com>
2026-03-10 18:05:51 +0530

    Time: 138 ms (43.64%), Space: 111.3 MB (18.54%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
-    int helper(vector<vector<int>>& dp, int i, int j, vector<int>& nums){
-        if(j==0) return 1;
-        if(i==0) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-        bool nottake=helper(dp, i-1, j, nums);
-        bool take=0;
-        if(nums[i-1]<=j) take=helper(dp, i-1, j-nums[i-1], nums);
-        return dp[i][j]=take||nottake;
-    }
-
     bool canPartition(vector<int>& nums) {
         int n=nums.size();
-        
         int sum=accumulate(nums.begin(), nums.end(), 0);
         if(sum%2) return 0;
         int k=sum/2;
         vector<vector<int>> dp(n+1, vector<int>(k+1, -1));
-        return helper(dp,n,k,nums);
+        return helper(dp, nums, n, k);
+    }
+
+    int helper(vector<vector<int>>& dp, vector<int>& nums, int i, int j){
+        if(j==0) return true;
+        if(i==0) return false;
+
+        if(dp[i][j]!=-1) return dp[i][j];
+
+        int nottake=helper(dp, nums, i-1, j);
+        int take=0;
+        if(nums[i-1]<=j) take=helper(dp, nums, i-1, j-nums[i-1]);
+
+        return dp[i][j]=take||nottake;
+
     }
 };
