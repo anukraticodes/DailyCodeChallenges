e83086693d74f01a543348488d39b1fb9df0dbda
Anukrati Chaturvedi <146018357+anukraticodes@users.noreply.github.com>
2026-03-11 14:21:28 +0530

    Time: 2 ms (81.93%), Space: 12 MB (71.55%) - LeetHub

--- 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.cpp
+++ 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.cpp
@@ -1,34 +1,25 @@
 class Solution {
 public:
-    bool solver(vector<int>& nums, vector<int>& visited, int start_index, int k, int current_sum, int target) {
-        if (k == 0) return true;  
-        if (current_sum == target) {
-            
-            return solver(nums, visited, 0, k - 1, 0, target);
-        }
-
-        for (int i = start_index; i < nums.size(); i++) {
-            if (!visited[i] && current_sum + nums[i] <= target) {
-                visited[i] = 1;  
-                if (solver(nums, visited, i + 1, k, current_sum + nums[i], target)) {
-                    return true;
-                }
-                visited[i] = 0;  
-               if (current_sum == 0)
-                    return false;
-            }
-        }
-
-        return false;
+    bool canPartitionKSubsets(vector<int>& nums, int k) {
+        int n=nums.size();
+        int sum=accumulate(nums.begin(), nums.end(), 0);
+        if(sum%k!=0) return 0;
+        int target=sum/k;
+        sort(nums.rbegin(), nums.rend());
+        vector<bool> vis(n, 0);
+        return helper(nums, vis, 0, k, 0, target);
     }
 
-    bool canPartitionKSubsets(vector<int>& nums, int k) {
-        int sum = accumulate(nums.begin(), nums.end(), 0);
-        if (sum % k != 0) return false;  
-
-        int target = sum / k;
-        vector<int> visited(nums.size(), 0);
-        sort(nums.begin(), nums.end(), greater<int>()); 
-        return solver(nums, visited, 0, k, 0, target);
+    int helper(vector<int>& nums, vector<bool>& vis, int start, int k, int currsum, int target){
+        if(k==1) return 1;
+        if(currsum==target) return helper(nums, vis, 0, k-1, 0, target);
+        for(int i=start; i<nums.size();i++){
+            if(vis[i]||currsum+nums[i]>target) continue;
+            vis[i]=1;
+            if(helper(nums, vis, i+1, k, currsum+nums[i], target)) return 1;
+            vis[i]=0;
+            if(currsum==0) break;
+        }
+        return 0;
     }
 };
