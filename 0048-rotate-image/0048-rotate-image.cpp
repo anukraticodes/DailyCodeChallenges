df00417fd94981d033a37f4ec440728119079b51
Anukrati Chaturvedi <146018357+anukraticodes@users.noreply.github.com>
2026-03-12 10:45:57 +0530

    Time: 0 ms (100%), Space: 10.1 MB (92.42%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,18 +1,15 @@
 class Solution {
 public:
-    void rotate(vector<vector<int>>& m) {
-        int n=m.size();
+    void rotate(vector<vector<int>>& matrix) {
+        int n=matrix.size(), m=matrix[0].size();
         for(int i=0; i<n; i++){
             for(int j=0; j<i; j++){
-                if(i!=j){
-                    swap(m[i][j],m[j][i]);
-                }
+                swap(matrix[i][j], matrix[j][i]);
             }
         }
-        for(int i=0; i<n/2; i++){
-            for(int j=0; j<n;j++){
-                swap(m[j][i], m[j][n-i-1]);
-            }
+        for(int i=0; i<n; i++){
+            reverse(matrix[i].begin(), matrix[i].end());
         }
+        return;
     }
 };
