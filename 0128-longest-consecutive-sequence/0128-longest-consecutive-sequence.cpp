80f20807bd9c7964a3c5a3f88aff5d67dbbf8478
Anukrati Chaturvedi <146018357+anukraticodes@users.noreply.github.com>
2026-03-18 18:03:35 +0530

    Time: 997 ms (5.1%), Space: 124.4 MB (5%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -1,18 +1,52 @@
 class Solution {
 public:
-    int longestConsecutive(vector<int>& nums) {
-        if(nums.size()==0) return 0;
-        sort(nums.begin(), nums.end());
-        int cnt=1, ans=1;
-        for(int i=0; i<nums.size()-1; i++){
-            if(nums[i]==nums[i+1]) continue;
-            if(nums[i+1]-nums[i]==1) cnt++;
+    struct DSU{
+        vector<int> parent, rank;
+        DSU(int n){
+            parent.resize(n);
+            rank.resize(n, 0);
+            for(int i=0; i<n; i++) parent[i]=i;
+        }
+
+        int findP(int u){
+            if(parent[u]!=u) return parent[u]=findP(parent[u]);
+            return parent[u]; 
+        }
+
+        void unionD(int u, int v){
+            int pu=findP(u), pv=findP(v);
+            if(pu==pv) return;
+            if(rank[pu]>rank[pv]){
+                parent[pv]=pu;
+            }
+            else if(rank[pv]>rank[pu]){
+                parent[pu]=pv;
+            }
             else{
-                ans=max(ans, cnt);
-                cnt=1;
+                parent[pu]=pv;
+                rank[pv]++;
             }
         }
-        ans=max(ans, cnt);
-        return ans;
+    };
+    int longestConsecutive(vector<int>& nums) {
+        unordered_map<int, int> mp;
+        for(int i=0; i<nums.size(); i++) mp[nums[i]]=i;
+        unordered_set<int> st;
+        int n=nums.size();
+        DSU dsu(n);
+        for(int i=0; i<nums.size(); i++){
+            int num=nums[i];
+            if(st.count(num)) continue;
+            if(mp.count(num-1)) dsu.unionD(mp[num], mp[num-1]);
+            if(mp.count(num+1)) dsu.unionD(mp[num], mp[num+1]);
+            st.insert(num);
+        }
+        vector<int> ans(n+1, 0);
+        for(auto it:mp){
+            int p=dsu.findP(it.second);
+            cout<<p<<endl;
+            ans[p]++;
+        }
+        return *max_element(ans.begin(), ans.end());
     }
 };
