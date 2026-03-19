6a2fab70169486fa49468a06d489766c5c6fef70
Anukrati Chaturvedi <146018357+anukraticodes@users.noreply.github.com>
2026-03-19 10:15:28 +0530

    Time: 0 ms (100%), Space: 14.1 MB (38.55%) - LeetHub

--- 0969-pancake-sorting/0969-pancake-sorting.cpp
+++ 0969-pancake-sorting/0969-pancake-sorting.cpp
@@ -1,24 +1,17 @@
 class Solution {
 public:
-    bool issorted(vector<int>& arr) {
-        for (int i = 0; i < arr.size() - 1; i++) {
-            if (arr[i] > arr[i + 1])
-                return 0;
-        }
-        return 1;
-    }
     vector<int> pancakeSort(vector<int>& arr) {
         int n = arr.size();
-        int k = n + 1;
         vector<int> ans;
-        while (k--) {
-            if (issorted(arr))
-                break;
+        for (int k = n; k >= 1; k--) {
             int ind = find(arr.begin(), arr.end(), k)-arr.begin();
-            reverse(arr.begin(), arr.begin() + 1 + ind);
-            reverse(arr.begin(), arr.begin() + k);
+            if(ind==k-1) continue;
+            if (ind != 0) {
+            reverse(arr.begin(), arr.begin() + ind + 1);
             ans.push_back(ind + 1);
-            ans.push_back(k);
+        }
+        reverse(arr.begin(), arr.begin() + k);
+        ans.push_back(k);
         }
         return ans;
     }
