aff307c81957fd6060066d57c051949bb30daee3
Anukrati Chaturvedi <146018357+anukraticodes@users.noreply.github.com>
2026-03-20 17:31:23 +0530

    Time: 3 ms (89.43%), Space: 23.7 MB (80.97%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -5,12 +5,12 @@ public:
         sort(intervals.begin(), intervals.end());
         ans.push_back(intervals[0]);
         for(int i=1; i<intervals.size(); i++){
-            if(intervals[i][0]<=ans.back()[1]){
-                ans.back()[1]=max(ans.back()[1], intervals[i][1]);
-            }
-            else{
-                ans.push_back(intervals[i]);
-            }
+           if(intervals[i][0]<=ans.back()[1]){
+            ans.back()[1]=max(ans.back()[1], intervals[i][1]);
+           }
+           else{
+            ans.push_back(intervals[i]);
+           }
         }
         return ans;
     }
