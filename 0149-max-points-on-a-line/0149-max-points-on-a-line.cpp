48163ac407a55b9d1529da8146b73d1978104a8b
Anukrati Chaturvedi <146018357+anukraticodes@users.noreply.github.com>
2026-03-21 12:17:59 +0530

    Time: 19 ms (78.24%), Space: 15.6 MB (74.04%) - LeetHub

--- 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
@@ -1,17 +1,31 @@
 class Solution {
 public:
     int maxPoints(vector<vector<int>>& points) {
-        if(points.size()<2) return points.size();
-        int ans=2;
-        for(int i=0; i<points.size(); i++){
-            for(int j=i+1; j<points.size(); j++){
-                int cnt=2;
-                for(int k=j+1; k<points.size(); k++){
-                    long long a=(points[k][1]-points[i][1])*(points[j][0]-points[i][0]);
-                    long long b=(points[k][0]-points[i][0])*(points[j][1]-points[i][1]);
-                    if(a==b) cnt++;
+        if (points.size() < 2)
+            return points.size();
+        int ans = 2;
+        for (int i = 0; i < points.size(); i++) {
+            map<pair<int, int>, int> mp;
+            for (int j = i + 1; j < points.size(); j++) {
+                int dy = (points[j][1] - points[i][1]);
+                int dx = (points[j][0] - points[i][0]);
+                int g = gcd(abs(dx), abs(dy));
+                dx /= g;
+                dy /= g;
+
+                if (dx == 0)
+                    dy = 1;
+                else if (dy == 0)
+                    dx = 1;
+                else if (dx < 0) {
+                    dx *= -1;
+                    dy *= -1;
                 }
-                ans=max(ans, cnt);
+                pair<int, int> curr = {dx, dy};
+                mp[curr]++;
+            }
+            for (auto it : mp) {
+                ans = max(ans, it.second + 1);
             }
         }
         return ans;
