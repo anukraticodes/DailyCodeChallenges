6f3e5807304ea7fa19f37e65ab1b68fe56a0cae0
Anukrati Chaturvedi <146018357+anukraticodes@users.noreply.github.com>
2026-03-23 17:31:23 +0530

    Time: 0 ms (100%), Space: 13.3 MB (44.55%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,25 +1,30 @@
 class Solution {
 public:
-    int bs(vector<int>& v, int target){
+    int bs1(vector<vector<int>>& v, int target){
         int l=0, r=v.size();
         while(l<r){
             int mid=(l+r)/2;
-            if(v[mid]>=target) r=mid;
+            if(v[mid][0]>=target) r=mid;
             else l=mid+1;
         }
         return l;
     }
-
+    int bs2(vector<vector<int>>& v, int target, int ind){
+        int l=0, r=v[0].size();
+        while(l<r){
+            int mid=(l+r)/2;
+            if(v[ind][mid]>=target) r=mid;
+            else l=mid+1;
+        }
+        return l;
+    }
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        vector<int> col;
-        for(int i=0; i<matrix.size(); i++) col.push_back(matrix[i][0]);
-        int ind=bs(col, target);
+        int ind=bs1(matrix, target);
      
         if(ind<matrix.size() && matrix[ind][0]==target) return 1;
         if(ind==0) return 0;
-        col.clear();
-        for(int i=0; i<matrix[0].size(); i++) col.push_back(matrix[ind-1][i]);
-        int ind2=bs(col, target);
+        
+        int ind2=bs2(matrix, target, ind-1);
         return (ind2<matrix[0].size() && matrix[ind-1][ind2]==target)?1:0;
     }
 };
