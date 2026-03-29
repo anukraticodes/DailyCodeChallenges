a9cacd0361497a1a0d57fc76c932b27213db6d9c
Anukrati Chaturvedi <146018357+anukraticodes@users.noreply.github.com>
2026-03-29 16:31:56 +0530

    Time: 173 ms (65.45%), Space: 302.4 MB (58.67%) - LeetHub

--- 3885-design-event-manager/3885-design-event-manager.cpp
+++ 3885-design-event-manager/3885-design-event-manager.cpp
@@ -1,31 +1,31 @@
 class EventManager {
 public:
-map<int, set<int>> mp;
-unordered_map<int, int> mp2;
+priority_queue<pair<int, int>> pq;
+unordered_map<int, int> mp;
     EventManager(vector<vector<int>>& events) {
         for(auto it:events){
             int id=it[0], p=it[1];
-            mp[p].insert(id);
-            mp2[id]=p;
+            pq.push({p, -id});
+            mp[id]=p;
         }
     }
     
     void updatePriority(int eventId, int newPriority) {
-        int p=mp2[eventId];
-        mp[p].erase(eventId);
-        if(mp[p].empty()) mp.erase(p);
-        mp[newPriority].insert(eventId);
-        mp2[eventId]=newPriority;
+        mp[eventId]=newPriority;
+        pq.push({newPriority, -eventId});
     }
     
     int pollHighest() {
-        if(!mp.empty()){
-        auto it=mp.rbegin();
-        auto k=*it->second.begin();
-        it->second.erase(k);
-        if(it->second.empty()) mp.erase(it->first);
-        mp2.erase(k);
-        return k;
+        while(!pq.empty()){
+        auto[p, id]=pq.top();
+        int nid=-id;
+
+        if(mp.count(nid) && mp[nid]==p){
+            pq.pop();
+            mp.erase(nid);
+            return nid;
+        }
+        pq.pop();
         }
         return -1;
     }
