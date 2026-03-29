e971cb5729b4a45e1e9d394eaa88c11322b3ebc3
Anukrati Chaturvedi <146018357+anukraticodes@users.noreply.github.com>
2026-03-29 10:09:41 +0530

    Time: 153 ms (23.53%), Space: 194.9 MB (27.64%) - LeetHub

--- 0460-lfu-cache/0460-lfu-cache.cpp
+++ 0460-lfu-cache/0460-lfu-cache.cpp
@@ -1,69 +1,59 @@
 class LFUCache {
 public:
-unordered_map<int, pair<int, int>> mp1;
-map<int, list<int>> mp2;
-unordered_map<int, list<int>::iterator> mp3;
-int cap;
+    unordered_map<int, pair<int, int>> mp1;
+    unordered_map<int, list<int>> mp2;
+    unordered_map<int, list<int>::iterator> mp3;
+    int cap, minf;
+
     LFUCache(int capacity) {
-        cap=capacity;
+        cap = capacity;
+        minf = 0;
     }
     
     int get(int key) {
-        if(mp1.count(key)){
-            auto[val, freq]=mp1[key];
-            auto it1=mp3[key];
-            mp2[freq].erase(it1);
-            mp2[freq+1].push_front(key);
-            auto it2=mp2[freq+1].begin();
-            mp3[key]=it2;
-            mp1[key].second=freq+1;
-            return val;
+        if (!mp1.count(key)) return -1;
+
+        auto [val, freq] = mp1[key];
+
+        // remove from old freq
+        mp2[freq].erase(mp3[key]);
+        if (mp2[freq].empty()) {
+            mp2.erase(freq);
+            if (minf == freq) minf++;
         }
-        return -1;
+
+        // add to new freq
+        mp2[freq + 1].push_front(key);
+        mp3[key] = mp2[freq + 1].begin();
+        mp1[key].second++;
+
+        return val;
     }
     
     void put(int key, int value) {
-        if(mp1.count(key)){
-            auto[val, freq]=mp1[key];
-            auto it1=mp3[key];
-            mp2[freq].erase(it1);
-            mp2[freq+1].push_front(key);
-            auto it2=mp2[freq+1].begin();
-            mp3[key]=it2;
-            mp1[key].second=freq+1;
-            mp1[key].first=value;
+        if (cap == 0) return;
+
+        if (mp1.count(key)) {
+            mp1[key].first = value;
+            get(key); // reuse logic
             return;
         }
-    
-        else if(mp1.size()<cap){
-            mp1[key]={value, 1};
-            mp2[1].push_front(key);
-            auto it=mp2[1].begin();
-            mp3[key]=it;
-            return;
-        }
-        else{
-            if(!mp2.empty()){
-                auto it=mp2.begin();
-                while(empty(it->second)) it++;
-                if(it==mp2.end()) return;
-                int k=it->second.back();
-                it->second.pop_back();
-                mp1.erase(k);
-                mp3.erase(k);
-                mp1[key]={value, 1};
-                mp2[1].push_front(key);
-                auto it2=mp2[1].begin();
-                mp3[key]=it2;
-                return;
+
+        if (mp1.size() == cap) {
+            int k = mp2[minf].back();
+            mp2[minf].pop_back();
+
+            if (mp2[minf].empty()) {
+                mp2.erase(minf);
             }
+
+            mp1.erase(k);
+            mp3.erase(k);
         }
+
+        mp1[key] = {value, 1};
+        mp2[1].push_front(key);
+        mp3[key] = mp2[1].begin();
+        minf = 1;
     }
 };
-
-/**
- * Your LFUCache object will be instantiated and called as such:
- * LFUCache* obj = new LFUCache(capacity);
- * int param_1 = obj->get(key);
- * obj->put(key,value);
- */
