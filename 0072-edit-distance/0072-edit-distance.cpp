af47f1b4405664244ce51c4b515cf57fba89bd7f
Anukrati Chaturvedi <146018357+anukraticodes@users.noreply.github.com>
2026-04-02 17:01:03 +0530

    Time: 7 ms (62.98%), Space: 14.6 MB (14.45%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,29 +1,17 @@
 class Solution {
 public:
-    int minDistance(string s, string t) {
-    int n = s.size(), m = t.size();
-    vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
-    return helper(s, t, n , m , dp);
-}
+    int minDistance(string word1, string word2) {
+        int n=word1.size(), m=word2.size();
+        vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
+        return helper(dp, word1, word2, n, m);
+    }
 
-int helper(const string& s, const string& t, int n, int m, vector<vector<int>>& dp) {
-       // if (i < 0) return j + 1;  
-    // if (j < 0) return i + 1;  
-    // if (dp[i][j] != -1) return dp[i][j];
-    for(int i=0; i<=n; i++){
-        dp[i][0]=i;
+    int helper(vector<vector<int>>& dp, string& w1, string& w2, int i, int j){
+        if(i==0) return j;
+        if(j==0) return i;
+        if(dp[i][j]!=-1) return dp[i][j];
+
+        if(w1[i-1]==w2[j-1]) return dp[i][j]=helper(dp, w1, w2, i-1, j-1);
+        return dp[i][j]=1+min({helper(dp, w1, w2, i-1, j), helper(dp, w1, w2, i, j-1), helper(dp, w1, w2, i-1, j-1)});
     }
-    for(int j=0; j<=m; j++){
-        dp[0][j]=j;
-    }
-    for(int i=1; i<=n; i++){
-        for(int j=1; j<=m; j++){
-            if(s[i-1]==t[j-1]) dp[i][j]=dp[i-1][j-1];
-            else{
-                dp[i][j]=1+min({dp[i-1][j-1], dp[i-1][j], dp[i][j-1]});
-            }
-        }
-    }
-    return dp[n][m];
-}
 };
