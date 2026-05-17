06b4e9c2957747705fd9ad83e9860a3a978a5529
Anukrati Chaturvedi <146018357+anukraticodes@users.noreply.github.com>
2026-05-17 18:52:29 +0530

    Time: 0 ms (100%), Space: 17.5 MB (53.04%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,47 +1,30 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        int n = (int)nums.size();
-        if (n == 0) return {-1, -1};
-
-        int st  = helper1(nums, target); // last index < target
-        int end = helper2(nums, target); // first index > target
-
-        int L = st + 1;
-        int R = end - 1;
-
-        if (L <= R && L >= 0 && R < n && nums[L] == target && nums[R] == target)
-            return {L, R};
-        return {-1, -1};
+        int n=nums.size();
+        int st=lb(nums, target);
+        if(st==n || nums[st]!=target) return {-1, -1};
+        int end=up(nums, target);
+        return {st, end-1};
     }
 
-    // last index with value < target
-    int helper1(vector<int>& nums, int target) {
-        int l = 0, r = (int)nums.size() - 1, ans = -1;
-        while (l <= r) {
-            int mid = l + (r - l) / 2;
-            if (nums[mid] < target) {
-                ans = mid;          // valid (< target)
-                l = mid + 1;        // push right to find last <
-            } else {
-                r = mid - 1;        // nums[mid] >= target
-            }
+    int lb(vector<int>& nums, int target){
+        int l=0, r=nums.size();
+        while(l<r){
+            int mid=(l+r)/2;
+            if(nums[mid]>=target) r=mid;
+            else l=mid+1;
         }
-        return ans; // -1 if none <
+        return l;
     }
 
-    // first index with value > target
-    int helper2(vector<int>& nums, int target) {
-        int l = 0, r = (int)nums.size() - 1, ans = (int)nums.size();
-        while (l <= r) {
-            int mid = l + (r - l) / 2;
-            if (nums[mid] > target) {
-                ans = mid;          // valid (> target)
-                r = mid - 1;        // pull left to find first >
-            } else {
-                l = mid + 1;        // nums[mid] <= target
-            }
+    int up(vector<int>& nums, int target){
+        int l=0, r=nums.size();
+        while(l<r){
+            int mid=(l+r)/2;
+            if(nums[mid]>target) r=mid;
+            else l=mid+1;
         }
-        return ans; // n if none >
+        return l;
     }
 };
