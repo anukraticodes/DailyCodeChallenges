714f14389c04825766f0802f8550e06ed3aff10c
Anukrati Chaturvedi <146018357+anukraticodes@users.noreply.github.com>
2026-05-17 19:00:48 +0530

    Time: 0 ms (100%), Space: 17.5 MB (87.8%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -2,27 +2,22 @@ class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
         int n=nums.size();
-        int st=lb(nums, target);
+        int st=bs(nums, target, 1);
         if(st==n || nums[st]!=target) return {-1, -1};
-        int end=up(nums, target);
+        int end=bs(nums, target, 0);
         return {st, end-1};
     }
 
-    int lb(vector<int>& nums, int target){
+    int bs(vector<int>& nums, int target, bool flag){
         int l=0, r=nums.size();
         while(l<r){
             int mid=(l+r)/2;
-            if(nums[mid]>=target) r=mid;
-            else l=mid+1;
-        }
-        return l;
-    }
-
-    int up(vector<int>& nums, int target){
-        int l=0, r=nums.size();
-        while(l<r){
-            int mid=(l+r)/2;
+           
             if(nums[mid]>target) r=mid;
+            
+            else if(flag==1 && nums[mid]==target){
+              r=mid;
+            }
             else l=mid+1;
         }
         return l;
